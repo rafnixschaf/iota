47f3d5a5e954ec239df822a3e23f2ac72d15b0be
Konstantinos Demartinos <konstantinos.demartinos@iota.org>
2024-04-18 13:35:43 +0300

    refactor(sui-genesis-builder): introduce FullSnapshotParser

--- crates/sui-genesis-builder/src/stardust/parse.rs
+++ crates/sui-genesis-builder/src/stardust/parse.rs
@@ -1,52 +1,41 @@
 //! Types and logic to parse a full Stardust snapshot.
-use std::{
-    fs::File,
-    io::{BufRead, Read},
-    path::Path,
-};
+use std::io::{BufReader, Read};
 
 use anyhow::Result;
 use iota_sdk::types::block::{output::Output, protocol::ProtocolParameters};
 use packable::PackableExt;
 
-use super::types::{FullSnapshotHeader, OutputHeader, TOTAL_SUPPLY_IOTA};
+use super::types::{FullSnapshotHeader, OutputHeader};
 
-/// Parse a full snapshot file located at `path` and iterate over the
-/// recorded [`Output`] values.
-pub fn parse_full_snapshot(path: impl AsRef<Path>) -> Result<()> {
-    let snapshot_file = File::open(path)?;
-    let mut reader = std::io::BufReader::new(snapshot_file);
-    let mut buf = [0_u8; FullSnapshotHeader::LENGTH];
-    reader.read_exact(&mut buf)?;
-
-    let full_header = FullSnapshotHeader::unpack_verified(buf.as_slice(), &())?;
-
-    println!("Output count: {}", full_header.output_count());
-
-    let total_supply = iterate_on_outputs(&mut reader, full_header.output_count())
-        .try_fold(0, |acc, output| {
-            Ok::<_, anyhow::Error>(acc + output?.amount())
-        })?;
-    assert_eq!(total_supply, TOTAL_SUPPLY_IOTA);
-    println!("Total supply: {total_supply}");
-    Ok(())
+/// Parse a full-snapshot using a [`BufReader`] internally.
+pub struct FullSnapshotParser<R: Read> {
+    reader: BufReader<R>,
+    /// The full-snapshot header
+    pub header: FullSnapshotHeader,
 }
 
-fn iterate_on_outputs(
-    src: &mut impl BufRead,
-    output_count: u64,
-) -> impl Iterator<Item = Result<Output, anyhow::Error>> + '_ {
-    let mut header_buf = [0_u8; OutputHeader::LENGTH];
-    let mut output_buf = [0_u8; u16::MAX as usize];
+impl<R: Read> FullSnapshotParser<R> {
+    pub fn new(reader: R) -> Result<Self> {
+        let mut reader = std::io::BufReader::new(reader);
+        let mut buf = [0_u8; FullSnapshotHeader::LENGTH];
+        reader.read_exact(&mut buf)?;
 
-    (0..output_count).map(move |_| {
-        src.read_exact(&mut header_buf)?;
-        let header = OutputHeader::unpack_verified(header_buf.as_slice(), &())?;
-        src.read_exact(&mut output_buf[0..header.length() as usize])?;
-        let output = Output::unpack_verified(
-            &output_buf[0..header.length() as usize],
-            &ProtocolParameters::default(),
-        )?;
-        Ok(output)
-    })
+        let header = FullSnapshotHeader::unpack_verified(buf.as_slice(), &())?;
+        Ok(Self { reader, header })
+    }
+
+    /// Provide an iterator over the Stardust UTXOs recorded in the snapshot.
+    pub fn outputs(mut self) -> impl Iterator<Item = Result<Output, anyhow::Error>> {
+        let mut header_buf = [0_u8; OutputHeader::LENGTH];
+        let mut output_buf = [0_u8; u16::MAX as usize];
+
+        (0..self.header.output_count()).map(move |_| {
+            self.reader.read_exact(&mut header_buf)?;
+            let header = OutputHeader::unpack_verified(header_buf.as_slice(), &())?;
+            let output_bytes = &mut output_buf[0..header.length() as usize];
+            self.reader.read_exact(output_bytes)?;
+            let output = Output::unpack_verified(output_bytes, &ProtocolParameters::default())?;
+            Ok(output)
+        })
+    }
 }
