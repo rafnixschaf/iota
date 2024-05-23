19d0f7183a93884a926db0b50c78242cb34eacef
Mario <mario.sarcevic@iota.org>
2024-05-23 12:44:56 +0200

    feat(tooling-general): Update prettier config (#375)
    * feat(tooling-general): Use 4 spaces for indentation
    
    * feat(tooling-general): Format config files in root after prettier change

--- prettier.config.js
+++ prettier.config.js
@@ -2,42 +2,42 @@
 // SPDX-License-Identifier: Apache-2.0
 
 module.exports = {
-	printWidth: 100,
-	semi: true,
-	singleQuote: true,
-	tabWidth: 2,
-	trailingComma: 'all',
-	useTabs: false,
-	plugins: ['@ianvs/prettier-plugin-sort-imports'],
-	importOrder: [
-		'<BUILT_IN_MODULES>',
-		'<THIRD_PARTY_MODULES>',
-		'',
-		'^@/(.*)$',
-		'^~/(.*)$',
-		'',
-		'^[.]',
-	],
-	importOrderParserPlugins: ["typescript", "decorators-legacy"],
-	overrides: [
-		{
-			files: 'apps/explorer/**/*',
-			options: {
-				plugins: ['prettier-plugin-tailwindcss'],
-				tailwindConfig: './apps/explorer/tailwind.config.ts',
-			},
-		},
-		{
-			files: 'sdk/**/*',
-			options: {
-				proseWrap: 'always',
-			},
-		},
-		{
-			files: 'external-crates/move/documentation/book/**/*',
-			options: {
-				proseWrap: 'always',
-			},
-		},
-	],
+    printWidth: 100,
+    semi: true,
+    singleQuote: true,
+    tabWidth: 4,
+    trailingComma: 'all',
+    useTabs: false,
+    plugins: ['@ianvs/prettier-plugin-sort-imports'],
+    importOrder: [
+        '<BUILT_IN_MODULES>',
+        '<THIRD_PARTY_MODULES>',
+        '',
+        '^@/(.*)$',
+        '^~/(.*)$',
+        '',
+        '^[.]',
+    ],
+    importOrderParserPlugins: ['typescript', 'decorators-legacy'],
+    overrides: [
+        {
+            files: 'apps/explorer/**/*',
+            options: {
+                plugins: ['prettier-plugin-tailwindcss'],
+                tailwindConfig: './apps/explorer/tailwind.config.ts',
+            },
+        },
+        {
+            files: 'sdk/**/*',
+            options: {
+                proseWrap: 'always',
+            },
+        },
+        {
+            files: 'external-crates/move/documentation/book/**/*',
+            options: {
+                proseWrap: 'always',
+            },
+        },
+    ],
 };
