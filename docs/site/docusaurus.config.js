c769e6bce213522e79fe2dfa42d374bbf52f602e
Dave de Fijter <dave@indentity.nl>
2024-05-24 14:34:32 +0200

    feat: Added initial docs for EVM to Move, syntax support for Solidity in the docs

--- docs/site/docusaurus.config.js
+++ docs/site/docusaurus.config.js
@@ -227,7 +227,7 @@ const config = {
       prism: {
         theme: themes.github,
         darkTheme: themes.nightOwl,
-        additionalLanguages: ["rust", "typescript", "toml"],
+        additionalLanguages: ["rust", "typescript", "toml", "solidity"],
       },
     }),
 };
