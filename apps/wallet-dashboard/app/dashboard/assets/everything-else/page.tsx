9706c70f2632ee36ddaa0c3ec9095172b02dbc85
evavirseda <evirseda@boxfish.studio>
2024-05-27 17:45:32 +0200

    feat(wallet-dashboard): add assets subroutes (#409)
    Co-authored-by: cpl121 <100352899+cpl121@users.noreply.github.com>

---
+++ apps/wallet-dashboard/app/dashboard/assets/everything-else/page.tsx
@@ -0,0 +1,12 @@
+// Copyright (c) 2024 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+function EverythingElsePage(): JSX.Element {
+    return (
+        <div className="flex items-center justify-center pt-12">
+            <h1>EVERYTHING ELSE</h1>
+        </div>
+    );
+}
+
+export default EverythingElsePage;
---
+++ apps/wallet-dashboard/app/dashboard/assets/virtual-assets/page.tsx
@@ -0,0 +1,12 @@
+// Copyright (c) 2024 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+function VirtualAssetsPage(): JSX.Element {
+    return (
+        <div className="flex items-center justify-center pt-12">
+            <h1>VIRTUAL ASSETS</h1>
+        </div>
+    );
+}
+
+export default VirtualAssetsPage;
