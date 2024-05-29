f1248ccb9a9d594ad8d87e93b3d472b226f23d1c
evavirseda <evirseda@boxfish.studio>
2024-05-29 15:48:24 +0200

    feat(wallet-dashboard): add visual assets UI (#429)
    * feat(wallet-dashboard): add virtual list
    
    * feat(wallet-dashboard): update items
    
    * feat(wallet-dashboard): add empty line and missing height
    
    * feat(wallet-dashboard): add missing empty line
    
    * feat(wallet-dashboard): add images to virtual assets
    
    * feat(wallet-dashboard): rename to visual assets
    
    ---------
    
    Co-authored-by: cpl121 <100352899+cpl121@users.noreply.github.com>

--- apps/wallet-dashboard/components/Box.tsx
+++ apps/wallet-dashboard/components/Box.tsx
@@ -4,18 +4,17 @@
 import React from 'react';
 
 interface BoxProps {
-    children: React.ReactNode
-    title?: string
+    children: React.ReactNode;
+    title?: string;
 }
 
 function Box({ children, title }: BoxProps): JSX.Element {
-
-	return (
-        <div className="flex flex-col gap-2 items-center p-4 rounded-lg border border-white">
+    return (
+        <div className="flex flex-col items-center gap-2 rounded-lg border border-white p-4">
             {title && <h2>{title}</h2>}
             {children}
         </div>
-    )
+    );
 }
 
 export default Box;
