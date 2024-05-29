123a0506c270fb504c2052b6f7562483c2b002cd
Bran <52735957+brancoder@users.noreply.github.com>
2024-05-29 12:17:34 +0200

    feat(wallet): Add import account from seed (#368)
    * feat: add import seed logic
    
    * feat: add export seed
    
    * fix: adjust styling for display box and remove eslint disable rules
    
    * feat: add seed validation
    
    * fix: remove comments and logs
    
    * Update apps/wallet/src/ui/app/hooks/useExportSeedMutation.tsx
    
    * chore: Deduplicate makeDerivationPath
    
    * chore: Clean up ExportSeedPage
    
    * chore: Remove unnecessary onClick
    
    ---------
    
    Co-authored-by: Marc Espin <mespinsanz@gmail.com>
    Co-authored-by: cpl121 <100352899+cpl121@users.noreply.github.com>

--- sdk/typescript/src/cryptography/mnemonics.ts
+++ sdk/typescript/src/cryptography/mnemonics.ts
@@ -1,5 +1,9 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
+
+// Modifications Copyright (c) 2024 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
 import { toHEX } from '@mysten/bcs';
 import { mnemonicToSeedSync as bip39MnemonicToSeedSync } from '@scure/bip39';
 
@@ -33,16 +37,16 @@ export function isValidBIP32Path(path: string): boolean {
 /**
  * Uses KDF to derive 64 bytes of key data from mnemonic with empty password.
  *
- * @param mnemonics 12 words string split by spaces.
+ * @param mnemonics 24 words string split by spaces.
  */
 export function mnemonicToSeed(mnemonics: string): Uint8Array {
 	return bip39MnemonicToSeedSync(mnemonics, '');
 }
 
 /**
- * Derive the seed in hex format from a 12-word mnemonic string.
+ * Derive the seed in hex format from a 24-word mnemonic string.
  *
- * @param mnemonics 12 words string split by spaces.
+ * @param mnemonics 24 words string split by spaces.
  */
 export function mnemonicToSeedHex(mnemonics: string): string {
 	return toHEX(mnemonicToSeed(mnemonics));
