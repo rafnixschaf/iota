8232295282e0c8d11d9a6ab54f1ffb7e98ea4bdc
JCNoguera <88061365+VmMad@users.noreply.github.com>
2024-05-31 08:04:34 +0200

    feat: add non visual assets (#461)
    Co-authored-by: cpl121 <100352899+cpl121@users.noreply.github.com>

--- apps/wallet-dashboard/app/dashboard/assets/everything-else/page.tsx
+++ apps/wallet-dashboard/app/dashboard/assets/everything-else/page.tsx
@@ -1,10 +1,62 @@
 // Copyright (c) 2024 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
+'use client';
+
+import { HARDCODED_NON_VISUAL_ASSETS } from '@/lib/mocks';
+import React from 'react';
+import { useVirtualizer } from '@tanstack/react-virtual';
+import { Box } from '@/components/index';
+
 function EverythingElsePage(): JSX.Element {
+    const containerRef = React.useRef(null);
+    const virtualizer = useVirtualizer({
+        count: HARDCODED_NON_VISUAL_ASSETS.length,
+        getScrollElement: () => containerRef.current,
+        estimateSize: () => 130,
+    });
+
+    const virtualItems = virtualizer.getVirtualItems();
     return (
-        <div className="flex items-center justify-center pt-12">
+        <div className="flex h-full w-full flex-col items-center justify-center space-y-4">
             <h1>EVERYTHING ELSE</h1>
+            <div className="relative h-[50vh] w-2/3 overflow-auto" ref={containerRef}>
+                <div
+                    style={{
+                        height: `${virtualizer.getTotalSize()}px`,
+                        width: '100%',
+                        position: 'relative',
+                    }}
+                >
+                    {virtualItems.map((virtualItem) => {
+                        const asset = HARDCODED_NON_VISUAL_ASSETS[virtualItem.index];
+                        return (
+                            <div
+                                key={virtualItem.key}
+                                className="absolute w-full pb-4 pr-4"
+                                style={{
+                                    position: 'absolute',
+                                    top: 0,
+                                    left: 0,
+                                    width: '100%',
+                                    height: `${virtualItem.size}px`,
+                                    transform: `translateY(${virtualItem.start}px)`,
+                                }}
+                            >
+                                <Box>
+                                    <div className="flex gap-2">
+                                        <div>
+                                            <p>Type: {asset.type}</p>
+                                            <p>Digest: {asset.digest}</p>
+                                            <p>Object ID: {asset.objectId}</p>
+                                        </div>
+                                    </div>
+                                </Box>
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
         </div>
     );
 }
---
+++ apps/wallet-dashboard/lib/mocks/non-visual-assets.mock.ts
@@ -0,0 +1,15 @@
+// Copyright (c) 2024 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+import { SuiObjectData } from '@mysten/sui.js/client';
+
+const NON_VISUAL_ASSET: SuiObjectData = {
+    digest: 'CvsV2zDm2THbQiuAD3PpHC9wizoANGLBZhJsQyaY88Zh',
+    objectId: '0x7da24c085e8940230e4853b2f6384be3c775b8f31f97be2f3b75995d7b692297',
+    version: '28331430',
+    type: '0x3::staking_pool::StakedSui',
+};
+
+export const HARDCODED_NON_VISUAL_ASSETS: SuiObjectData[] = new Array(10)
+    .fill(0)
+    .map(() => NON_VISUAL_ASSET);
