b055b6fcb9c7089d77b20fe14b458a983bd2c05f
evavirseda <evirseda@boxfish.studio>
2024-05-31 08:25:58 +0200

    feat(wallet-dashboard): add activity detail popup UI (#437)
    * feat(wallet-dashboard): add ActivityDetailsPopup component
    
    * feat(wallet-dashboard): use formatTime function
    
    ---------
    
    Co-authored-by: cpl121 <100352899+cpl121@users.noreply.github.com>

--- apps/wallet-dashboard/components/ActivityTile.tsx
+++ apps/wallet-dashboard/components/ActivityTile.tsx
@@ -7,20 +7,28 @@ import React from 'react';
 import ActivityIcon from './ActivityIcon';
 import formatTimestamp from '@/lib/utils/time';
 import { Activity } from '@/lib/interfaces';
+import { usePopups } from '@/hooks';
+import { ActivityDetailsPopup, Button } from '@/components';
 
 interface ActivityTileProps {
     activity: Activity;
 }
 
-function ActivityTile({ activity: { action, state, timestamp } }: ActivityTileProps): JSX.Element {
+function ActivityTile({ activity }: ActivityTileProps): JSX.Element {
+    const { openPopup, closePopup } = usePopups();
+
+    const handleDetailsClick = () => {
+        openPopup(<ActivityDetailsPopup activity={activity} onClose={closePopup} />);
+    };
+
     return (
         <div className="border-gray-45 flex h-full w-full flex-row items-center space-x-4 rounded-md border border-solid p-4">
-            <ActivityIcon state={state} action={action} />
-            <div className="flex h-full flex-col space-y-2">
-                <h2>{action}</h2>
-                <span>{formatTimestamp(timestamp)}</span>
+            <ActivityIcon state={activity.state} action={activity.action} />
+            <div className="flex h-full w-full flex-col space-y-2">
+                <h2>{activity.action}</h2>
+                <span>{formatTimestamp(activity.timestamp)}</span>
             </div>
-            <hr />
+            <Button onClick={handleDetailsClick}>Details</Button>
         </div>
     );
 }
--- apps/wallet-dashboard/hooks/index.ts
+++ apps/wallet-dashboard/hooks/index.ts
@@ -2,4 +2,4 @@
 // SPDX-License-Identifier: Apache-2.0
 
 export * from './useBalance';
-export * from './usePopup';
+export * from './usePopups';
