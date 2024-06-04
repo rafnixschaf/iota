f89c739c40d63f41f544b56d23614e56b7e7591e
Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>
2024-06-04 12:29:03 +0200

    feat(sui-genesis-builder): Add Nft Output object validation (#444)
    * feat(sui-genesis-builder): Add Nft Output object validation
    
    * Fix native_token check and migrate tag without size prefix
    
    * Remove unused import
    
    * Refactor verify_native_tokens() and improve error messages
    
    * Cleanup imports, fmt
    
    * Handle coins and fields
    
    * Check native token bag size
    
    * Update alias
    
    * Revert native_tokens() return type change
    
    ---------
    
    Co-authored-by: DaughterOfMars <alexandcoats@gmail.com>

--- crates/sui-genesis-builder/src/stardust/migration/verification/basic.rs
+++ crates/sui-genesis-builder/src/stardust/migration/verification/basic.rs
@@ -5,7 +5,9 @@ use std::collections::HashMap;
 
 use anyhow::{anyhow, ensure, Result};
 use iota_sdk::types::block::output::{BasicOutput, TokenId};
-use sui_types::{balance::Balance, dynamic_field::Field, in_memory_storage::InMemoryStorage};
+use sui_types::{
+    balance::Balance, coin::Coin, dynamic_field::Field, in_memory_storage::InMemoryStorage, TypeTag,
+};
 
 use crate::stardust::migration::{
     executor::FoundryLedgerData,
@@ -47,28 +49,12 @@ pub(super) fn verify_basic_output(
         );
 
         // Native Tokens
-        ensure!(
-            created_output.native_tokens.size == output.native_tokens().len() as u64,
-            "native tokens bag length mismatch: found {}, expected {}",
-            created_output.native_tokens.size,
-            output.native_tokens().len()
-        );
-        let created_native_token_fields = created_objects.native_tokens().and_then(|ids| {
-            ids.iter()
-                .map(|id| {
-                    let obj = storage
-                        .get_object(id)
-                        .ok_or_else(|| anyhow!("missing native token field for {id}"))?;
-                    obj.to_rust::<Field<String, Balance>>().ok_or_else(|| {
-                        anyhow!("expected a native token field, found {:?}", obj.type_())
-                    })
-                })
-                .collect::<Result<Vec<_>, _>>()
-        })?;
-        verify_native_tokens(
+        verify_native_tokens::<Field<String, Balance>>(
             output.native_tokens(),
             foundry_data,
-            created_native_token_fields,
+            created_output.native_tokens,
+            created_objects.native_tokens().ok(),
+            storage,
         )?;
 
         // Storage Deposit Return Unlock Condition
@@ -128,24 +114,12 @@ pub(super) fn verify_basic_output(
         );
 
         // Native Tokens
-        let created_native_token_coins = created_objects.native_tokens().and_then(|ids| {
-            ids.iter()
-                .map(|id| {
-                    let obj = storage
-                        .get_object(id)
-                        .ok_or_else(|| anyhow!("missing native token coin for {id}"))?;
-                    obj.coin_type_maybe()
-                        .zip(obj.as_coin_maybe())
-                        .ok_or_else(|| {
-                            anyhow!("expected a native token coin, found {:?}", obj.type_())
-                        })
-                })
-                .collect::<Result<Vec<_>, _>>()
-        })?;
-        verify_native_tokens(
+        verify_native_tokens::<(TypeTag, Coin)>(
             output.native_tokens(),
             foundry_data,
-            created_native_token_coins,
+            None,
+            created_objects.native_tokens().ok(),
+            storage,
         )?;
     }
 
--- crates/sui-genesis-builder/src/stardust/migration/verification/nft.rs
+++ crates/sui-genesis-builder/src/stardust/migration/verification/nft.rs
@@ -3,21 +3,111 @@
 
 use std::collections::HashMap;
 
-use iota_sdk::types::block::output::{NftOutput, TokenId};
-use sui_types::in_memory_storage::InMemoryStorage;
+use anyhow::{anyhow, ensure};
+use iota_sdk::types::block::output::{NftOutput, OutputId, TokenId};
+use sui_types::{
+    balance::Balance, base_types::ObjectID, dynamic_field::Field,
+    in_memory_storage::InMemoryStorage,
+};
 
 use crate::stardust::migration::{
     executor::FoundryLedgerData,
-    verification::{created_objects::CreatedObjects, util::verify_parent},
+    verification::{
+        created_objects::CreatedObjects,
+        util::{
+            verify_expiration_unlock_condition, verify_issuer_feature, verify_metadata_feature,
+            verify_native_tokens, verify_parent, verify_sender_feature,
+            verify_storage_deposit_unlock_condition, verify_tag_feature,
+            verify_timelock_unlock_condition,
+        },
+    },
 };
 
-pub(super) fn verify_nft_output(
+pub(crate) fn verify_nft_output(
+    output_id: OutputId,
     output: &NftOutput,
-    _created_objects: &CreatedObjects,
-    _foundry_data: &HashMap<TokenId, FoundryLedgerData>,
+    created_objects: &CreatedObjects,
+    foundry_data: &HashMap<TokenId, FoundryLedgerData>,
     storage: &InMemoryStorage,
 ) -> anyhow::Result<()> {
-    // TODO: Implementation. Returns Ok for now so the migration can be tested.
+    let created_output = created_objects
+        .output()
+        .and_then(|id| {
+            storage
+                .get_object(id)
+                .ok_or_else(|| anyhow!("missing nft output object for {output_id}"))
+        })?
+        .to_rust::<crate::stardust::types::NftOutput>()
+        .ok_or_else(|| anyhow!("invalid nft output object for {output_id}"))?;
+
+    let nft = storage
+        .get_object(&ObjectID::new(*output.nft_id_non_null(&output_id)))
+        .ok_or_else(|| anyhow!("missing nft object for {output_id}"))?
+        .to_rust::<crate::stardust::types::Nft>()
+        .ok_or_else(|| anyhow!("invalid nft object for {output_id}"))?;
+
+    // Amount
+    ensure!(
+        created_output.iota.value() == output.amount(),
+        "amount mismatch: found {}, expected {}",
+        created_output.iota.value(),
+        output.amount()
+    );
+
+    // Native Tokens
+    verify_native_tokens::<Field<String, Balance>>(
+        output.native_tokens(),
+        foundry_data,
+        created_output.native_tokens,
+        created_objects.native_tokens().ok(),
+        storage,
+    )?;
+
+    // Storage Deposit Return Unlock Condition
+    verify_storage_deposit_unlock_condition(
+        output.unlock_conditions().storage_deposit_return(),
+        created_output.storage_deposit_return.as_ref(),
+    )?;
+
+    // Timelock Unlock Condition
+    verify_timelock_unlock_condition(
+        output.unlock_conditions().timelock(),
+        created_output.timelock.as_ref(),
+    )?;
+
+    // Expiration Unlock Condition
+    verify_expiration_unlock_condition(
+        output.unlock_conditions().expiration(),
+        created_output.expiration.as_ref(),
+        output.address(),
+    )?;
+
+    // Metadata Feature
+    verify_metadata_feature(output.features().metadata(), nft.metadata.as_ref())?;
+
+    // Tag Feature
+    verify_tag_feature(output.features().tag(), nft.tag.as_ref())?;
+
+    // Sender Feature
+    verify_sender_feature(output.features().sender(), nft.legacy_sender)?;
+
+    // Issuer Feature
+    verify_issuer_feature(output.immutable_features().issuer(), nft.immutable_issuer)?;
+
+    // Immutable Metadata Feature
+    ensure!(
+        crate::stardust::types::Nft::convert_immutable_metadata(output)? == nft.immutable_metadata,
+        "metadata mismatch: found {:x?}, expected {:x?}",
+        crate::stardust::types::Nft::convert_immutable_metadata(output)?,
+        nft.immutable_metadata
+    );
+
+    ensure!(
+        created_objects.package().is_err(),
+        "unexpected package found"
+    );
+
     verify_parent(output.address(), storage)?;
+
     Ok(())
 }
