bf914d8f45a4f111a5cfc583e4a79295a04ce80c
Marc Espin <mespinsanz@gmail.com>
2024-06-06 16:24:27 +0200

    refactor(wallet): Format after rename (#560)
    * refactor(wallet): Format after rename
    
    * refactor(wallet): Reverse old license header change
    
    ---------
    
    Co-authored-by: cpl121 <100352899+cpl121@users.noreply.github.com>

--- apps/wallet/src/ui/app/components/ledger/ConnectLedgerModal.tsx
+++ apps/wallet/src/ui/app/components/ledger/ConnectLedgerModal.tsx
@@ -58,8 +58,8 @@ export function ConnectLedgerModal({ onClose, onConfirm, onError }: ConnectLedge
                     </div>
                     <div className="mt-4.5 break-words text-center">
                         <Text variant="pBodySmall" color="steel-darker" weight="normal">
-                            Connect your ledger to your computer, unlock it, and launch the Iota app.
-                            Click Continue when done.
+                            Connect your ledger to your computer, unlock it, and launch the Iota
+                            app. Click Continue when done.
                         </Text>
                         <div className="mt-2 flex items-center justify-center">
                             <Text variant="pBodySmall" color="steel-dark" weight="normal">
