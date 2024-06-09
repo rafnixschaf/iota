bb1a75b897e2f025cf9caad0abaf9d2cafa4da58
Jeroen van den Hout <jlvandenhout@gmail.com>
2024-06-09 23:52:03 +0200

    feat(docs): add ISC docs

--- docs/content/sidebars/guides.js
+++ docs/content/sidebars/guides.js
@@ -184,6 +184,426 @@ const guides = [
 					'guides/developer/app-examples/turnip-town',
 				],
 			},
+			{
+				type: 'category',
+				label: 'Layer 2 Smart Contracts',
+				link: {
+					type: 'doc',
+					id: 'guides/developer/layer-2-smart-contracts/introduction',
+				},
+				items: [
+					{
+						type: 'doc',
+						label: 'Introduction',
+						id: 'guides/developer/layer-2-smart-contracts/introduction',
+					},
+					{
+						type: 'category',
+						label: 'Getting Started',
+						items: [
+							{
+								type: 'doc',
+								label: 'Languages & VMs',
+								id: 'guides/developer/layer-2-smart-contracts/getting-started/languages-and-vms',
+							},
+							'getting-started/quick-start',
+							'getting-started/compatibility',
+							{
+								type: 'doc',
+								label: 'Networks & Chains',
+								id: 'guides/developer/layer-2-smart-contracts/getting-started/networks-and-chains',
+							},
+							{
+								type: 'doc',
+								label: 'Tools',
+								id: 'guides/developer/layer-2-smart-contracts/getting-started/tools',
+							},
+						],
+					},
+					{
+						type: 'category',
+						label: 'How To',
+						items: [
+							'how-tos/introduction',
+							{
+								type: 'doc',
+								label: 'Send Funds from L1 to L2',
+								id: 'guides/developer/layer-2-smart-contracts/how-tos/send-funds-from-L1-to-L2',
+							},
+							{
+								type: 'doc',
+								label: 'Create a Basic Contract',
+								id: 'guides/developer/layer-2-smart-contracts/how-tos/create-a-basic-contract',
+							},
+							{
+								type: 'doc',
+								label: 'Deploy a Smart Contract',
+								id: 'guides/developer/layer-2-smart-contracts/how-tos/deploy-a-smart-contract',
+							},
+							{
+								type: 'doc',
+								label: 'Create Custom Tokens - ERC20',
+								id: 'guides/developer/layer-2-smart-contracts/how-tos/ERC20',
+							},
+							{
+								type: 'doc',
+								label: 'Send ERC20 Tokens Across Chains',
+								id: 'guides/developer/layer-2-smart-contracts/how-tos/send-ERC20-across-chains',
+							},
+							{
+								type: 'doc',
+								label: 'Create NFTs - ERC721',
+								id: 'guides/developer/layer-2-smart-contracts/how-tos/ERC721',
+							},
+							{
+								type: 'doc',
+								label: 'Send NFTs Across Chains',
+								id: 'guides/developer/layer-2-smart-contracts/how-tos/send-NFTs-across-chains',
+							},
+							{
+								type: 'doc',
+								label: 'Test Smart Contracts',
+								id: 'guides/developer/layer-2-smart-contracts/how-tos/test-smart-contracts',
+							},
+							{
+								type: 'category',
+								label: 'Interact with the Core Contracts',
+								items: [
+									{
+										type: 'doc',
+										label: 'Introduction',
+										id: 'guides/developer/layer-2-smart-contracts/how-tos/core-contracts/introduction',
+									},
+									{
+										type: 'category',
+										label: 'Basics',
+										items: [
+											{
+												type: 'doc',
+												label: 'Get Native Assets Balance',
+												id: 'guides/developer/layer-2-smart-contracts/how-tos/core-contracts/basics/get-balance',
+											},
+											{
+												type: 'category',
+												label: 'Allowance',
+												items: [
+													{
+														type: 'doc',
+														label: 'Allow',
+														id: 'guides/developer/layer-2-smart-contracts/how-tos/core-contracts/basics/allowance/allow',
+													},
+													{
+														type: 'doc',
+														label: 'Get Allowance',
+														id: 'guides/developer/layer-2-smart-contracts/how-tos/core-contracts/basics/allowance/get-allowance',
+													},
+													{
+														type: 'doc',
+														label: 'Take Allowance',
+														id: 'guides/developer/layer-2-smart-contracts/how-tos/core-contracts/basics/allowance/take-allowance',
+													},
+												],
+											},
+											{
+												type: 'doc',
+												label: 'Send Assets to L1',
+												id: 'guides/developer/layer-2-smart-contracts/how-tos/core-contracts/basics/send-assets-to-l1',
+											},
+										],
+									},
+									{
+										type: 'category',
+										label: 'Token',
+										items: [
+											{
+												label: 'Introduction',
+												type: 'doc',
+												id: 'guides/developer/layer-2-smart-contracts/how-tos/core-contracts/token/introduction',
+											},
+											{
+												type: 'doc',
+												label: 'Create a Native Token',
+												id: 'guides/developer/layer-2-smart-contracts/how-tos/core-contracts/token/create-native-token',
+											},
+											{
+												type: 'doc',
+												label: 'Mint Native Tokens',
+												id: 'guides/developer/layer-2-smart-contracts/how-tos/core-contracts/token/mint-token',
+											},
+											{
+												type: 'doc',
+												label: 'Custom ERC20 Functions',
+												id: 'guides/developer/layer-2-smart-contracts/how-tos/core-contracts/token/erc20-native-token',
+											},
+											{
+												type: 'doc',
+												label: 'Create a Foundry',
+												id: 'guides/developer/layer-2-smart-contracts/how-tos/core-contracts/token/create-foundry',
+											},
+											{
+												type: 'doc',
+												label: 'Register Token as ERC20',
+												id: 'guides/developer/layer-2-smart-contracts/how-tos/core-contracts/token/register-token',
+											},
+										],
+									},
+									{
+										type: 'category',
+										label: 'NFT',
+										items: [
+											{
+												label: 'Introduction',
+												type: 'doc',
+												id: 'guides/developer/layer-2-smart-contracts/how-tos/core-contracts/nft/introduction',
+											},
+											{
+												type: 'doc',
+												label: 'Mint an NFT',
+												id: 'guides/developer/layer-2-smart-contracts/how-tos/core-contracts/nft/mint-nft',
+											},
+											{
+												type: 'doc',
+												label: 'Use as ERC721',
+												id: 'guides/developer/layer-2-smart-contracts/how-tos/core-contracts/nft/use-as-erc721',
+											},
+										],
+									},
+									{
+										type: 'doc',
+										label: 'Get Randomness on L2',
+										id: 'guides/developer/layer-2-smart-contracts/how-tos/core-contracts/get-randomness-on-l2',
+									},
+									{
+										type: 'doc',
+										label: 'Call and Call View',
+										id: 'guides/developer/layer-2-smart-contracts/how-tos/core-contracts/call-view',
+									},
+								],
+							},
+						],
+					},
+					{
+						type: 'category',
+						label: 'Explanations',
+						items: [
+							{
+								type: 'doc',
+								label: 'Anatomy of a Smart Contract',
+								id: 'guides/developer/layer-2-smart-contracts/explanations/smart-contract-anatomy',
+							},
+							{
+								type: 'doc',
+								label: 'Sandbox Interface',
+								id: 'guides/developer/layer-2-smart-contracts/explanations/sandbox',
+							},
+							{
+								type: 'doc',
+								label: 'Calling a Smart Contract',
+								id: 'guides/developer/layer-2-smart-contracts/explanations/invocation',
+							},
+							{
+								type: 'doc',
+								label: 'State, Transitions and State Anchoring',
+								id: 'guides/developer/layer-2-smart-contracts/explanations/states',
+							},
+							{
+								type: 'doc',
+								label: 'State manager',
+								id: 'guides/developer/layer-2-smart-contracts/explanations/state_manager',
+							},
+							{
+								type: 'doc',
+								label: 'Validators and Access Nodes',
+								id: 'guides/developer/layer-2-smart-contracts/explanations/validators',
+							},
+							{
+								type: 'doc',
+								label: 'Consensus',
+								id: 'guides/developer/layer-2-smart-contracts/explanations/consensus',
+							},
+							{
+								type: 'doc',
+								label: 'How Accounts Work',
+								id: 'guides/developer/layer-2-smart-contracts/explanations/how-accounts-work',
+							},
+							{
+								type: 'link',
+								label: 'Core Contracts',
+								href: '/isc/reference/core-contracts/overview',
+							},
+						],
+					},
+					{
+						type: 'category',
+						label: 'Test with Solo',
+						items: [
+							{
+								label: 'Getting Started',
+								id: 'guides/developer/layer-2-smart-contracts/solo/getting-started',
+								type: 'doc',
+							},
+							{
+								type: 'category',
+								label: 'How To',
+								items: [
+									{
+										type: 'doc',
+										label: 'First Example',
+										id: 'guides/developer/layer-2-smart-contracts/solo/how-tos/first-example',
+									},
+									{
+										type: 'doc',
+										label: 'The L1 Ledger',
+										id: 'guides/developer/layer-2-smart-contracts/solo/how-tos/the-l1-ledger',
+									},
+									{
+										type: 'doc',
+										label: 'Deploy a Smart Contract',
+										id: 'guides/developer/layer-2-smart-contracts/solo/how-tos/deploying-sc',
+									},
+									{
+										type: 'doc',
+										label: 'Invoke a Smart Contract',
+										id: 'guides/developer/layer-2-smart-contracts/solo/how-tos/invoking-sc',
+									},
+									{
+										type: 'doc',
+										label: 'Call a View',
+										id: 'guides/developer/layer-2-smart-contracts/solo/how-tos/view-sc',
+									},
+									{
+										type: 'doc',
+										label: 'Error Handling',
+										id: 'guides/developer/layer-2-smart-contracts/solo/how-tos/error-handling',
+									},
+									{
+										type: 'doc',
+										label: 'Accounts',
+										id: 'guides/developer/layer-2-smart-contracts/solo/how-tos/the-l2-ledger',
+									},
+									{
+										type: 'doc',
+										label: 'Test Smart Contracts',
+										id: 'guides/developer/layer-2-smart-contracts/solo/how-tos/test',
+									},
+									{
+										type: 'doc',
+										label: 'Example Tests',
+										id: 'guides/developer/layer-2-smart-contracts/solo/how-tos/examples',
+									},
+									{
+										type: 'doc',
+										label: 'Colored Tokens and Time Locks',
+										id: 'guides/developer/layer-2-smart-contracts/solo/how-tos/timelock',
+									},
+								],
+							},
+						],
+					},
+					{
+						type: 'category',
+						label: 'Wasm - Schema Tool',
+						items: [
+							{
+								type: 'doc',
+								label: 'The Schema Tool',
+								id: 'guides/developer/layer-2-smart-contracts/schema/introduction',
+							},
+							{
+								type: 'doc',
+								label: 'Data Access Proxies',
+								id: 'guides/developer/layer-2-smart-contracts/schema/proxies',
+							},
+							{
+								type: 'category',
+								label: 'How To',
+								items: [
+									{
+										type: 'doc',
+										label: 'Create a Schema',
+										id: 'guides/developer/layer-2-smart-contracts/schema/how-tos/usage',
+									},
+									{
+										type: 'doc',
+										label: 'Define the State',
+										id: 'guides/developer/layer-2-smart-contracts/schema/how-tos/state',
+									},
+									{
+										type: 'doc',
+										label: 'Use Structured Data Types',
+										id: 'guides/developer/layer-2-smart-contracts/schema/how-tos/structs',
+									},
+									{
+										type: 'doc',
+										label: 'Generate Type Definitions',
+										id: 'guides/developer/layer-2-smart-contracts/schema/how-tos/typedefs',
+									},
+									{
+										type: 'doc',
+										label: 'Trigger Events',
+										id: 'guides/developer/layer-2-smart-contracts/schema/how-tos/events',
+									},
+									{
+										type: 'doc',
+										label: 'Define Functions',
+										id: 'guides/developer/layer-2-smart-contracts/schema/how-tos/funcs',
+									},
+									{
+										type: 'doc',
+										label: 'Limit Access',
+										id: 'guides/developer/layer-2-smart-contracts/schema/how-tos/access',
+									},
+									{
+										type: 'doc',
+										label: 'Define Function Parameters',
+										id: 'guides/developer/layer-2-smart-contracts/schema/how-tos/params',
+									},
+									{
+										type: 'doc',
+										label: 'Define Function Results',
+										id: 'guides/developer/layer-2-smart-contracts/schema/how-tos/results',
+									},
+									{
+										type: 'doc',
+										label: 'Use Thunk Functions',
+										id: 'guides/developer/layer-2-smart-contracts/schema/how-tos/thunks',
+									},
+									{
+										type: 'doc',
+										label: 'Use View-Only Functions',
+										id: 'guides/developer/layer-2-smart-contracts/schema/how-tos/views',
+									},
+									{
+										type: 'doc',
+										label: 'Initialize a Smart Contract',
+										id: 'guides/developer/layer-2-smart-contracts/schema/how-tos/init',
+									},
+									{
+										type: 'doc',
+										label: 'Transfer Tokens',
+										id: 'guides/developer/layer-2-smart-contracts/schema/how-tos/transfers',
+									},
+									{
+										type: 'doc',
+										label: 'Add Function Descriptors',
+										id: 'guides/developer/layer-2-smart-contracts/schema/how-tos/funcdesc',
+									},
+									{
+										type: 'doc',
+										label: 'Call Functions',
+										id: 'guides/developer/layer-2-smart-contracts/schema/how-tos/call',
+									},
+									{
+										type: 'doc',
+										label: 'Post Asynchronous Requests',
+										id: 'guides/developer/layer-2-smart-contracts/schema/how-tos/post',
+									},
+								],
+							},
+						],
+					},
+				],
+			},
 			'guides/developer/starter-templates',
 			'guides/developer/zklogin-onboarding',
 			'guides/developer/dev-cheat-sheet',
@@ -205,6 +625,62 @@ const guides = [
 			'guides/operator/genesis',
 			'guides/operator/validator-committee',
 			'guides/operator/validator-tasks',
+			{
+				type: 'category',
+				label: 'Layer 2 Smart Contracts Node',
+				link: {
+					type: 'doc',
+					id: 'guides/operator/layer-2-smart-contracts-node/how-tos/running-a-node',
+				},
+				items: [
+					{
+						type: 'category',
+						label: 'How To',
+						collapsed: false,
+						items: [
+							{
+								type: 'doc',
+								id: 'guides/operator/layer-2-smart-contracts-node/how-tos/running-a-node',
+								label: 'Run a Node',
+							},
+							{
+								type: 'doc',
+								id: 'guides/operator/layer-2-smart-contracts-node/how-tos/running-an-access-node',
+								label: 'Run an Access Node',
+							},
+							{
+								id: 'guides/operator/layer-2-smart-contracts-node/how-tos/wasp-cli',
+								label: 'Configure wasp-cli',
+								type: 'doc',
+							},
+							{
+								id: 'guides/operator/layer-2-smart-contracts-node/how-tos/setting-up-a-chain',
+								label: 'Set Up a Chain',
+								type: 'doc',
+							},
+							{
+								id: 'guides/operator/layer-2-smart-contracts-node/how-tos/chain-management',
+								label: 'Manage a Chain',
+								type: 'doc',
+							},
+						],
+					},
+					{
+						type: 'category',
+						label: 'Reference',
+						items: [
+							{
+								type: 'doc',
+								id: 'guides/operator/layer-2-smart-contracts-node/reference/configuration',
+							},
+							{
+								type: 'doc',
+								id: 'guides/operator/layer-2-smart-contracts-node/reference/metrics',
+							},
+						],
+					},
+				],
+			},
 			'guides/operator/node-tools',
 			'guides/operator/exchange-integration',
 		],
