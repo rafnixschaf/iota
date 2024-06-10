88667017eb18cd686cf6586e505ecf8440b26aed
Bran <52735957+brancoder@users.noreply.github.com>
2024-06-10 17:03:07 +0200

    feat(wallet): Add password recovery for seed account source (#466)
    * feat: add password recovery for seed account source
    
    * fix: import seed form styling
    
    * fix(wallet): merge mnemonic and seed flags in recover many page
    
    * fix: display account panels only if they exist
    
    * fix: resolve conflicts
    
    ---------
    
    Co-authored-by: cpl121 <100352899+cpl121@users.noreply.github.com>
    Co-authored-by: Begoña Álvarez de la Cruz <balvarez@boxfish.studio>

--- apps/wallet/src/ui/app/pages/accounts/forgot-password/RecoverPage.tsx
+++ apps/wallet/src/ui/app/pages/accounts/forgot-password/RecoverPage.tsx
@@ -12,49 +12,71 @@ import { useNavigate } from 'react-router-dom';
 import { useAccountSources } from '../../../hooks/useAccountSources';
 import { Heading } from '../../../shared/heading';
 import { Text } from '../../../shared/text';
+import { ImportSeedForm } from '_src/ui/app/components/accounts/ImportSeedForm';
 
 export function RecoverPage() {
     const allAccountSources = useAccountSources();
     const navigate = useNavigate();
     const mnemonicAccountSource = allAccountSources.data?.find(({ type }) => type === 'mnemonic');
+    const seedAccountSource = allAccountSources.data?.find(({ type }) => type === 'seed');
     useEffect(() => {
-        if (!allAccountSources.isPending && !mnemonicAccountSource) {
+        if (!allAccountSources.isPending && !mnemonicAccountSource && !seedAccountSource) {
             navigate('/', { replace: true });
         }
-    }, [allAccountSources.isPending, mnemonicAccountSource, navigate]);
+    }, [allAccountSources.isPending, mnemonicAccountSource, seedAccountSource, navigate]);
     const recoveryDataMutation = useRecoveryDataMutation();
-    if (!mnemonicAccountSource) {
+    if (!mnemonicAccountSource && !seedAccountSource) {
         return null;
     }
+    const title = mnemonicAccountSource
+        ? 'Recover with 24-word Recovery Phrase'
+        : 'Recover with Seed';
     return (
-        <div className="flex flex-1 flex-col items-center gap-6">
+        <div className="flex w-full flex-1 flex-col items-center gap-6">
             <div className="flex flex-col items-center gap-2">
                 <Heading variant="heading1" color="gray-90" as="h1" weight="bold">
                     Forgot Password?
                 </Heading>
                 <Text variant="pBody" color="gray-90">
-                    Enter your 24-word Recovery Phrase
+                    {title}
                 </Text>
             </div>
-            <div className="grow">
-                <ImportRecoveryPhraseForm
-                    cancelButtonText="Cancel"
-                    submitButtonText="Next"
-                    onSubmit={async ({ recoveryPhrase }) => {
-                        try {
-                            await recoveryDataMutation.mutateAsync({
-                                type: 'mnemonic',
-                                accountSourceID: mnemonicAccountSource.id,
-                                entropy: entropyToSerialized(
-                                    mnemonicToEntropy(recoveryPhrase.join(' ')),
-                                ),
-                            });
-                            navigate('../warning');
-                        } catch (e) {
-                            toast.error((e as Error)?.message || 'Something went wrong');
-                        }
-                    }}
-                />
+            <div className="w-full grow">
+                {mnemonicAccountSource ? (
+                    <ImportRecoveryPhraseForm
+                        cancelButtonText="Cancel"
+                        submitButtonText="Next"
+                        onSubmit={async ({ recoveryPhrase }) => {
+                            try {
+                                await recoveryDataMutation.mutateAsync({
+                                    type: 'mnemonic',
+                                    accountSourceID: mnemonicAccountSource.id,
+                                    entropy: entropyToSerialized(
+                                        mnemonicToEntropy(recoveryPhrase.join(' ')),
+                                    ),
+                                });
+                                navigate('../warning');
+                            } catch (e) {
+                                toast.error((e as Error)?.message || 'Something went wrong');
+                            }
+                        }}
+                    />
+                ) : (
+                    <ImportSeedForm
+                        onSubmit={async ({ seed }) => {
+                            try {
+                                await recoveryDataMutation.mutateAsync({
+                                    type: 'seed',
+                                    accountSourceID: seedAccountSource?.id ?? '',
+                                    seed,
+                                });
+                                navigate('../warning');
+                            } catch (e) {
+                                toast.error((e as Error)?.message || 'Something went wrong');
+                            }
+                        }}
+                    />
+                )}
             </div>
         </div>
     );
