ea6179103ff75cbec2c2763d8d00a21b188ccc29
evavirseda <evirseda@boxfish.studio>
2024-06-10 18:28:56 +0200

    feat(wallet-dashboard): add asset details subroute UI (#484)
    * feat(wallet-dashboard): add asset details subroute
    
    * feat(wallet-dashboard): rename prop
    
    * feat(wallet-dashboard): rename prop
    
    * feat(wallet-dashboard):cleanup
    
    * feat(wallet-dashboard): use AssetCard in asset details route
    
    ---------
    
    Co-authored-by: cpl121 <100352899+cpl121@users.noreply.github.com>
    Co-authored-by: Begoña Alvarez <balvarez@boxfish.studio>
    Co-authored-by: Bran <52735957+brancoder@users.noreply.github.com>

--- apps/wallet-dashboard/app/dashboard/assets/everything-else/page.tsx
+++ apps/wallet-dashboard/app/dashboard/assets/everything-else/page.tsx
@@ -7,21 +7,28 @@ import { HARDCODED_NON_VISUAL_ASSETS } from '@/lib/mocks';
 import React from 'react';
 import { IotaObjectData } from '@iota/iota.js/client';
 import { AssetCard, VirtualList } from '@/components/index';
+import { useRouter } from 'next/navigation';
 
 function EverythingElsePage(): JSX.Element {
+    const router = useRouter();
+
     const virtualItem = (asset: IotaObjectData): JSX.Element => (
         <AssetCard key={asset.objectId} asset={asset} />
     );
 
+    const handleClick = (objectId: string) => {
+        router.push(`/dashboard/assets/everything-else/${objectId}`);
+    };
+
     return (
         <div className="flex h-full w-full flex-col items-center justify-center space-y-4">
             <h1>EVERYTHING ELSE</h1>
-
             <div className="flex w-1/2">
                 <VirtualList
                     items={HARDCODED_NON_VISUAL_ASSETS}
-                    estimateSize={() => 130}
+                    estimateSize={() => 140}
                     render={virtualItem}
+                    onClick={(asset) => handleClick(asset.objectId)}
                 />
             </div>
         </div>
