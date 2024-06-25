ba685f23574e76c72b80ddbfd2a74ae67df9f722
Samuel Rufinatscha <samuel.rufinatscha@iota.org>
2024-06-25 20:24:09 +0200

    fix(iota-genesis-builder): Don't suppress BasicOutput metadata, tag, and sender features (#820)
    * fix: Fix `is_simple_coin`
    
    * fix: Use `stardust_to_iota_address`
    
    * fix: Basic verification
    
    * refactor: Simplify expression
    
    * fix: Fix wrong condition nesting
    
    ---------
    
    Co-authored-by: DaughterOfMars <alexandcoats@gmail.com>

--- crates/iota-genesis-builder/src/stardust/migration/verification/basic.rs
+++ crates/iota-genesis-builder/src/stardust/migration/verification/basic.rs
@@ -86,8 +86,8 @@ pub(super) fn verify_basic_output(
         return Ok(());
     }
 
-    // If the output has multiple unlock conditions, then a genesis object should
-    // have been created.
+    // If the output has multiple unlock conditions or a metadata, tag or sender
+    // feature, then a genesis object should have been created.
     if output.unlock_conditions().expiration().is_some()
         || output
             .unlock_conditions()
@@ -96,6 +96,7 @@ pub(super) fn verify_basic_output(
         || output
             .unlock_conditions()
             .is_time_locked(target_milestone_timestamp)
+        || !output.features().is_empty()
     {
         ensure!(created_objects.coin().is_err(), "unexpected coin created");
 
