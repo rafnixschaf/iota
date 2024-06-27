305a5999364096a4f5842b57a8c00983495df2a2
Thibault Martinez <thibault@iota.org>
2024-06-27 10:56:55 +0200

    feat(iota-genesis-builder): vesting schedule entity test data (#837)
    * concat vectors
    
    * new mnemonic
    
    * only one address
    
    * cleanup
    
    * 4 years of vesting
    
    * factorize some code
    
    * randomness seed log
    
    * Use random_output_index()
    
    ---------
    
    Co-authored-by: /alex/ <alexander.schmidt@iota.org>
    Co-authored-by: Thoralf Müller <thoralf.mue@gmail.com>
    Co-authored-by: Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>

--- crates/iota-genesis-builder/src/stardust/test_outputs/mod.rs
+++ crates/iota-genesis-builder/src/stardust/test_outputs/mod.rs
@@ -1,6 +1,7 @@
 // Copyright (c) 2024 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
+mod vesting_schedule_entity;
 mod vesting_schedule_iota_airdrop;
 
 use std::{
@@ -10,13 +11,54 @@ use std::{
     str::FromStr,
 };
 
-use iota_sdk::types::block::output::{BasicOutputBuilder, Output, OutputId};
+use iota_sdk::types::block::{
+    address::Ed25519Address,
+    output::{
+        unlock_condition::{AddressUnlockCondition, TimelockUnlockCondition},
+        BasicOutputBuilder, Output, OutputId,
+    },
+};
 use packable::{packer::IoPacker, Packable};
 
-use crate::stardust::parse::HornetGenesisSnapshotParser;
+use crate::stardust::{
+    parse::HornetGenesisSnapshotParser,
+    types::{output_header::OutputHeader, output_index::random_output_index},
+};
 
 const OUTPUT_TO_DECREASE_AMOUNT_FROM: &str =
     "0xb462c8b2595d40d3ff19924e3731f501aab13e215613ce3e248d0ed9f212db160000";
+const MERGE_MILESTONE_INDEX: u32 = 7669900;
+const MERGE_TIMESTAMP_SECS: u32 = 1696406475;
+
+pub(crate) fn new_vested_output(
+    transaction_id: &mut [u8; 32],
+    vested_index: &mut u32,
+    amount: u64,
+    address: Ed25519Address,
+    timelock: Option<u32>,
+) -> anyhow::Result<(OutputHeader, Output)> {
+    transaction_id[28..32].copy_from_slice(&vested_index.to_le_bytes());
+    *vested_index -= 1;
+
+    let output_header = OutputHeader::new_testing(
+        *transaction_id,
+        random_output_index(),
+        [0; 32],
+        MERGE_MILESTONE_INDEX,
+        MERGE_TIMESTAMP_SECS,
+    );
+
+    let mut builder = BasicOutputBuilder::new_with_amount(amount)
+        .add_unlock_condition(AddressUnlockCondition::new(address));
+
+    if let Some(timelock) = timelock {
+        builder = builder.add_unlock_condition(TimelockUnlockCondition::new(timelock)?);
+    }
+
+    let output = Output::from(builder.finish().unwrap());
+
+    Ok((output_header, output))
+}
 
 /// Adds outputs to test specific and intricate scenario in the full snapshot.
 pub async fn add_snapshot_test_outputs<P: AsRef<Path> + core::fmt::Debug>(
@@ -35,7 +77,11 @@ pub async fn add_snapshot_test_outputs<P: AsRef<Path> + core::fmt::Debug>(
     let mut new_header = parser.header.clone();
     let mut vested_index = u32::MAX;
 
-    let new_outputs = vesting_schedule_iota_airdrop::outputs(&mut vested_index).await?;
+    let new_outputs = [
+        vesting_schedule_entity::outputs(&mut vested_index).await?,
+        vesting_schedule_iota_airdrop::outputs(&mut vested_index).await?,
+    ]
+    .concat();
     let new_amount = new_outputs.iter().map(|o| o.1.amount()).sum::<u64>();
 
     // Increments the output count according to newly generated outputs.
