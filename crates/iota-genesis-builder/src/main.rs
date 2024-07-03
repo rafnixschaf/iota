53b55ec0271d3e369e7b61775516970174508763
/alex/ <alexander.schmidt@iota.org>
2024-07-03 09:34:13 +0200

    feat(iota-genesis-builder): add cli command to disable global snapshot verification (#911)
    * add cli command to disable global snapshot verification
    
    * refactor: Pass `VERIFY` generic type
    
    * refactor: Remove generic type from outputs()
    
    * move comment
    
    ---------
    
    Co-authored-by: samuel_rufi <samuel.rufinatscha@iota.org>

--- crates/iota-genesis-builder/src/main.rs
+++ crates/iota-genesis-builder/src/main.rs
@@ -14,7 +14,7 @@ use clap::{Parser, Subcommand};
 use iota_genesis_builder::{
     stardust::{
         migration::{Migration, MigrationTargetNetwork},
-        parse::HornetGenesisSnapshotParser,
+        parse::HornetSnapshotParser,
     },
     BROTLI_COMPRESSOR_BUFFER_SIZE, BROTLI_COMPRESSOR_LG_WINDOW_SIZE, BROTLI_COMPRESSOR_QUALITY,
     OBJECT_SNAPSHOT_FILE_PATH,
@@ -40,6 +40,12 @@ struct Cli {
         help = "Compress the resulting object snapshot"
     )]
     compress: bool,
+    #[clap(
+        long,
+        help = "Enable global snapshot verification",
+        default_value_t = true
+    )]
+    global_snapshot_verification: bool,
 }
 
 #[derive(Subcommand, Debug)]
@@ -81,7 +87,11 @@ fn main() -> Result<()> {
     };
 
     // Start the Hornet snapshot parser
-    let mut snapshot_parser = HornetGenesisSnapshotParser::new(File::open(snapshot_path)?)?;
+    let mut snapshot_parser = if cli.global_snapshot_verification {
+        HornetSnapshotParser::new::<true>(File::open(snapshot_path)?)?
+    } else {
+        HornetSnapshotParser::new::<false>(File::open(snapshot_path)?)?
+    };
     let total_supply = match coin_type {
         CoinType::Iota => scale_amount_for_iota(snapshot_parser.total_supply()?)?,
         CoinType::Shimmer => snapshot_parser.total_supply()?,
@@ -121,6 +131,7 @@ fn main() -> Result<()> {
             }
         })
         .process_results(|outputs| migration.run(outputs, object_snapshot_writer))??;
+
     Ok(())
 }
 
