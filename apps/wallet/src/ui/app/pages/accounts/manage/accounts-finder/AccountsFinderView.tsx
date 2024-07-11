ede4ef0236a08126d5e70e7e2076246a50472004
Marc Espin <mespinsanz@gmail.com>
2024-07-11 15:37:01 +0200

    feat(wallet): Unlock accounts upon searching in Account Finder (#1018)
    * feat(wallet): Unlock accounts upon opening account finder
    
    * fix and clean up
    
    * search after pwd verify

--- apps/wallet/src/ui/app/pages/accounts/manage/accounts-finder/AccountsFinderView.tsx
+++ apps/wallet/src/ui/app/pages/accounts/manage/accounts-finder/AccountsFinderView.tsx
@@ -11,6 +11,9 @@ import { type AllowedAccountTypes } from '_src/background/accounts-finder';
 import { useAccounts } from '_src/ui/app/hooks/useAccounts';
 import { getKey } from '_src/ui/app/helpers/accounts';
 import { useState } from 'react';
+import { VerifyPasswordModal } from '_src/ui/app/components/accounts/VerifyPasswordModal';
+import { useAccountSources } from '_src/ui/app/hooks/useAccountSources';
+import { useUnlockMutation } from '_src/ui/app/hooks/useUnlockMutation';
 
 export function AccountsFinderView(): JSX.Element {
     const { accountSourceId } = useParams();
@@ -23,34 +26,58 @@ export function AccountsFinderView(): JSX.Element {
         accountType: currentAccount?.type as AllowedAccountTypes,
         sourceID: accountSourceId || '',
     });
+    const { data: accountSources } = useAccountSources();
+    const unlockAccountSourceMutation = useUnlockMutation();
+    const [isPasswordModalVisible, setPasswordModalVisible] = useState(false);
+    const accountSource = accountSources?.find(({ id }) => id === accountSourceId);
 
     function searchMore() {
-        setSearched(true);
-        search();
+        if (accountSource?.isLocked) {
+            setPasswordModalVisible(true);
+        } else {
+            setSearched(true);
+            search();
+        }
     }
 
     return (
-        <div className="flex h-full flex-1 flex-col justify-between">
-            <div className="flex h-96 flex-col gap-4 overflow-y-auto">
-                {persistedAccounts?.map((account) => {
-                    return <AccountBalanceItem key={account.id} account={account} />;
-                })}
-            </div>
-            <div className="flex flex-col gap-2">
-                <Button variant="outline" size="tall" text={'Start again'} onClick={reset} />
-                <Button
-                    variant="outline"
-                    size="tall"
-                    text={searched ? 'Search again' : 'Search'}
-                    after={<Search24 />}
-                    onClick={searchMore}
-                />
+        <>
+            <div className="flex h-full flex-1 flex-col justify-between">
+                <div className="flex h-96 flex-col gap-4 overflow-y-auto">
+                    {persistedAccounts?.map((account) => {
+                        return <AccountBalanceItem key={account.id} account={account} />;
+                    })}
+                </div>
+                <div className="flex flex-col gap-2">
+                    <Button variant="outline" size="tall" text={'Start again'} onClick={reset} />
+                    <Button
+                        variant="outline"
+                        size="tall"
+                        text={searched ? 'Search again' : 'Search'}
+                        after={<Search24 />}
+                        onClick={searchMore}
+                    />
 
-                <div className="flex flex-row gap-2">
-                    <Button variant="outline" size="tall" text="Skip" />
-                    <Button variant="outline" size="tall" text="Continue" />
+                    <div className="flex flex-row gap-2">
+                        <Button variant="outline" size="tall" text="Skip" />
+                        <Button variant="outline" size="tall" text="Continue" />
+                    </div>
                 </div>
             </div>
-        </div>
+            {isPasswordModalVisible && accountSourceId ? (
+                <VerifyPasswordModal
+                    open
+                    onVerify={async (password) => {
+                        await unlockAccountSourceMutation.mutateAsync({
+                            id: accountSourceId,
+                            password,
+                        });
+                        setPasswordModalVisible(false);
+                        search();
+                    }}
+                    onClose={() => setPasswordModalVisible(false)}
+                />
+            ) : null}
+        </>
     );
 }
