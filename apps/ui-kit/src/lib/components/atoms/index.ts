dfd9763ff4b0c992321124b1b96c3a719436d1f8
JCNoguera <88061365+VmMad@users.noreply.github.com>
2024-07-17 12:36:33 +0200

    feat(apps-ui-kit): add checkbox component (#1161)
    * feat(apps-ui-kit): add checkbox component
    
    * refactor: use enabled and rounded clases
    
    * refactor: rename booleans to follow convention
    
    * refactor: use the icons from internal package
    
    * refactor: handle label first with flex row reverse
    
    ---------
    
    Co-authored-by: Bran <52735957+brancoder@users.noreply.github.com>

--- apps/ui-kit/src/lib/components/atoms/index.ts
+++ apps/ui-kit/src/lib/components/atoms/index.ts
@@ -3,10 +3,10 @@
 
 export * from './address';
 export * from './badge';
-export * from './button';
 export * from './button-segment';
-export * from './header';
+export * from './button';
+export * from './checkbox';
 export * from './chip';
 export * from './divider';
-export * from './labelText';
 export * from './header';
+export * from './labelText';
