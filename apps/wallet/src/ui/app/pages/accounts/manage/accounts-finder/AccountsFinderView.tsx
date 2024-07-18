e5f58b36c24162ee5ca598c40c66c4908b42a4e3
Marc Espin <mespinsanz@gmail.com>
2024-07-18 16:47:50 +0200

    feat(wallet): Support `Ledger` in Accounts Finder (#1175)
    * feat(wallet): test support with speculos ledger simulator over http transport
    
    * feat(tooling-wallet): Polish speculos transport solution (tx with speculos ledger working!)
    
    * feat(tooling-wallet): Revert requestPermissionsFirst back to true
    
    * feat(tooling-wallet): Revert useless function placement change
    
    * feat(tooling-wallet): Small comment update
    
    * feat(tooling-wallet): Use default speculos API port (5000)
    
    * fix(tooling-wallet): Fix header in new file
    
    * feat(tooling-wallet): Let's move the speculos api default PORT to a constant
    
    * refactor(wallet): reorganize files
    
    * feat(wallet): `Ledger` support for Accounts Finder
    
    * fmt
    
    * clean up helpers.ts
    
    * clean up imports
    
    * clean up imports again
    
    * modals
    
    * bug fixes and improvements
    
    * reverse .gitignore
    
    * accountSourceType
    
    * proper redirec
    
    * bug fixes and improvements x2
    
    * bring happiness to the CI
    
    * fix test
    
    * small fix
    
    * comment
    
    * fmt
    
    * remove reset method
    
    * memoize source strategy
    
    * improve a little bit the ux
    
    * fix(wallet): Use correct URL for Ledger redirect to Account Finder from onboarding
    
    ---------
    
    Co-authored-by: Begoña Alvarez <balvarez@boxfish.studio>
    Co-authored-by: Mario Sarcevic <mario.sarcevic@iota.org>

--- apps/wallet/src/ui/app/pages/accounts/manage/accounts-finder/AccountsFinderView.tsx
+++ apps/wallet/src/ui/app/pages/accounts/manage/accounts-finder/AccountsFinderView.tsx
@@ -6,39 +6,85 @@ import { Button } from '_src/ui/app/shared/ButtonUI';
 import { AccountBalanceItem } from '_src/ui/app/components/accounts/AccountBalanceItem';
 import { useAccountsFinder } from '_src/ui/app/hooks/useAccountsFinder';
 import { useParams } from 'react-router-dom';
-import { useActiveAccount } from '_app/hooks/useActiveAccount';
-import { type AllowedAccountTypes } from '_src/background/accounts-finder';
+import { AllowedAccountSourceTypes } from '_src/ui/app/accounts-finder';
 import { useAccounts } from '_src/ui/app/hooks/useAccounts';
 import { getKey } from '_src/ui/app/helpers/accounts';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import { VerifyPasswordModal } from '_src/ui/app/components/accounts/VerifyPasswordModal';
 import { useAccountSources } from '_src/ui/app/hooks/useAccountSources';
 import { useUnlockMutation } from '_src/ui/app/hooks/useUnlockMutation';
+import { AccountType } from '_src/background/accounts/Account';
+import { ConnectLedgerModal } from '_src/ui/app/components/ledger/ConnectLedgerModal';
+import toast from 'react-hot-toast';
+import { getLedgerConnectionErrorMessage } from '_src/ui/app/helpers/errorMessages';
+import { useIotaLedgerClient } from '_src/ui/app/components/ledger/IotaLedgerClientProvider';
+import {
+    AccountSourceType,
+    type AccountSourceSerializedUI,
+} from '_src/background/account-sources/AccountSource';
+import { type SourceStrategyToFind } from '_src/shared/messaging/messages/payloads/accounts-finder';
+
+function getAccountSourceType(
+    accountSource?: AccountSourceSerializedUI,
+): AllowedAccountSourceTypes {
+    switch (accountSource?.type) {
+        case AccountSourceType.Mnemonic:
+            return AllowedAccountSourceTypes.MnemonicDerived;
+        case AccountSourceType.Seed:
+            return AllowedAccountSourceTypes.SeedDerived;
+        default:
+            return AllowedAccountSourceTypes.LedgerDerived;
+    }
+}
 
 export function AccountsFinderView(): JSX.Element {
     const { accountSourceId } = useParams();
-    const { data: accounts } = useAccounts();
-    const persistedAccounts = accounts?.filter((acc) => getKey(acc) === accountSourceId);
-    const currentAccount = useActiveAccount();
-    const [searched, setSearched] = useState(false);
-
-    const { search, reset } = useAccountsFinder({
-        accountType: currentAccount?.type as AllowedAccountTypes,
-        sourceID: accountSourceId || '',
-    });
     const { data: accountSources } = useAccountSources();
-    const unlockAccountSourceMutation = useUnlockMutation();
-    const [isPasswordModalVisible, setPasswordModalVisible] = useState(false);
+    const { data: accounts } = useAccounts();
     const accountSource = accountSources?.find(({ id }) => id === accountSourceId);
+    const accountSourceType = getAccountSourceType(accountSource);
+    const [searched, setSearched] = useState(false);
+    const [password, setPassword] = useState('');
+    const [isPasswordModalVisible, setPasswordModalVisible] = useState(false);
+    const [isConnectLedgerModalOpen, setConnectLedgerModalOpen] = useState(false);
+    const ledgerIotaClient = useIotaLedgerClient();
+    const unlockAccountSourceMutation = useUnlockMutation();
+    const sourceStrategy: SourceStrategyToFind = useMemo(
+        () =>
+            accountSourceType == AllowedAccountSourceTypes.LedgerDerived
+                ? {
+                      type: 'ledger',
+                      password,
+                  }
+                : {
+                      type: 'software',
+                      sourceID: accountSourceId!,
+                  },
+        [password, accountSourceId],
+    );
+    const { find } = useAccountsFinder({
+        accountSourceType,
+        sourceStrategy,
+    });
 
-    function searchMore() {
-        if (accountSource?.isLocked) {
-            setPasswordModalVisible(true);
-        } else {
-            setSearched(true);
-            search();
-        }
+    function unlockLedger() {
+        setConnectLedgerModalOpen(true);
     }
+
+    function verifyPassword() {
+        setPasswordModalVisible(true);
+    }
+
+    async function findMore() {
+        await find();
+        setSearched(true);
+    }
+
+    const persistedAccounts = accounts?.filter((acc) => getKey(acc) === accountSourceId);
+    const isLocked =
+        accountSource?.isLocked || (accountSourceId === AccountType.LedgerDerived && !password);
+    const isLedgerLocked =
+        accountSourceId === AccountType.LedgerDerived && !ledgerIotaClient.iotaLedgerClient;
 
     return (
         <>
@@ -49,35 +95,75 @@ export function AccountsFinderView(): JSX.Element {
                     })}
                 </div>
                 <div className="flex flex-col gap-2">
-                    <Button variant="outline" size="tall" text={'Start again'} onClick={reset} />
-                    <Button
-                        variant="outline"
-                        size="tall"
-                        text={searched ? 'Search again' : 'Search'}
-                        after={<Search24 />}
-                        onClick={searchMore}
-                    />
+                    {isLedgerLocked ? (
+                        <Button
+                            variant="outline"
+                            size="tall"
+                            text="Unlock Ledger"
+                            onClick={unlockLedger}
+                        />
+                    ) : isLocked ? (
+                        <Button
+                            variant="outline"
+                            size="tall"
+                            text="Verify password"
+                            onClick={verifyPassword}
+                        />
+                    ) : (
+                        <>
+                            <Button
+                                variant="outline"
+                                size="tall"
+                                text={searched ? 'Search again' : 'Search'}
+                                after={<Search24 />}
+                                onClick={findMore}
+                            />
 
-                    <div className="flex flex-row gap-2">
-                        <Button variant="outline" size="tall" text="Skip" />
-                        <Button variant="outline" size="tall" text="Continue" />
-                    </div>
+                            <div className="flex flex-row gap-2">
+                                <Button variant="outline" size="tall" text="Skip" />
+                                <Button variant="outline" size="tall" text="Continue" />
+                            </div>
+                        </>
+                    )}
                 </div>
             </div>
-            {isPasswordModalVisible && accountSourceId ? (
+            {isPasswordModalVisible ? (
                 <VerifyPasswordModal
                     open
                     onVerify={async (password) => {
-                        await unlockAccountSourceMutation.mutateAsync({
-                            id: accountSourceId,
-                            password,
-                        });
+                        if (accountSourceType === AllowedAccountSourceTypes.LedgerDerived) {
+                            // for ledger
+                            setPassword(password);
+                        } else if (accountSourceId) {
+                            // unlock software account sources
+                            await unlockAccountSourceMutation.mutateAsync({
+                                id: accountSourceId,
+                                password,
+                            });
+                        }
+
                         setPasswordModalVisible(false);
-                        search();
                     }}
                     onClose={() => setPasswordModalVisible(false)}
                 />
             ) : null}
+            {isConnectLedgerModalOpen && (
+                <ConnectLedgerModal
+                    onClose={() => {
+                        setConnectLedgerModalOpen(false);
+                    }}
+                    onError={(error) => {
+                        setConnectLedgerModalOpen(false);
+                        toast.error(
+                            getLedgerConnectionErrorMessage(error) || 'Something went wrong.',
+                        );
+                    }}
+                    onConfirm={() => {
+                        setConnectLedgerModalOpen(false);
+                        setPasswordModalVisible(true);
+                    }}
+                />
+            )}
         </>
     );
 }
