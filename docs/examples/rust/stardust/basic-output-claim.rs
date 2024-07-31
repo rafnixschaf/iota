09e09b28e36220030f058ae976a2c6bb3ec18847
Mirko Zichichi <miker83z@proton.me>
2024-07-31 16:08:16 +0200

    fix(docs/examples): move basic output example

--- docs/examples/rust/stardust/basic-output-claim.rs
+++ docs/examples/rust/stardust/basic-output-claim.rs
@@ -59,6 +59,8 @@ async fn main() -> Result<(), anyhow::Error> {
     // Derive the address of the first account and set it as default
     let sender = keystore.import_from_mnemonic(MAIN_ADDRESS_MNEMONIC, ED25519, None)?;
 
+    println!("Sender address: {sender:?}");
+
     // Get a gas coin
     let gas_coin = iota_client
         .coin_read_api()
