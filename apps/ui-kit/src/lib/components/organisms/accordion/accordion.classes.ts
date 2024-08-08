0e867899fdf2159dc366fc271ffdcc03f3138818
Eugene P. <panteleymonchuk@gmail.com>
2024-08-08 19:00:34 +0300

    feat(apps-ui-kit): add Accordion component (#1640)
    * feat(apps-ui-kit): Accordion.
    
    Signed-off-by: Eugene Panteleymonchuk <panteleymonchuk@gmail.com>
    
    * feat(apps-ui-kit): Accordion. Add arrow color.
    
    Signed-off-by: Eugene Panteleymonchuk <panteleymonchuk@gmail.com>
    
    * feat(apps-ui-kit): Accordion. Composition.
    
    Signed-off-by: Eugene Panteleymonchuk <panteleymonchuk@gmail.com>
    
    * feat(apps-ui-kit): Accordion. Updates.
    
    Signed-off-by: Eugene Panteleymonchuk <panteleymonchuk@gmail.com>
    
    * feat(apps-ui-kit): Snackbar. Add border.
    
    Signed-off-by: Eugene Panteleymonchuk <panteleymonchuk@gmail.com>
    
    * feat(apps-ui-kit): Snackbar. Add border color.
    
    Signed-off-by: Eugene Panteleymonchuk <panteleymonchuk@gmail.com>
    
    * feat(apps-ui-kit): Snackbar. Add dark color.
    
    Signed-off-by: Eugene Panteleymonchuk <panteleymonchuk@gmail.com>
    
    ---------
    
    Signed-off-by: Eugene Panteleymonchuk <panteleymonchuk@gmail.com>

---
+++ apps/ui-kit/src/lib/components/organisms/accordion/accordion.classes.ts
@@ -0,0 +1,5 @@
+// Copyright (c) 2024 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+export const ICON_STYLE =
+    'h-5 w-5 text-neutral-10 dark:text-neutral-92 transition-transform ease-linear';
