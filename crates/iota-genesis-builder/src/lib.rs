0dd7151d0daa35eaf69920572b942c6b88c6b0d1
tomxey <tpastusiak@gmail.com>
2024-08-08 11:48:23 +0200

    feat(iota-genesis-builder): Remove brotli compression flag in genesis builder (#1651)
    * feat(iota-genesis-builder): Remove brotli compression flag in genesis builder
    
    * Remove the annotation for --compress
    
    * Remove brotli dependency from iota-genesis-builder

--- crates/iota-genesis-builder/src/lib.rs
+++ crates/iota-genesis-builder/src/lib.rs
@@ -84,12 +84,6 @@ const GENESIS_BUILDER_SIGNATURE_DIR: &str = "signatures";
 const GENESIS_BUILDER_UNSIGNED_GENESIS_FILE: &str = "unsigned-genesis";
 const GENESIS_BUILDER_MIGRATION_SOURCES_FILE: &str = "migration-sources";
 
-pub const BROTLI_COMPRESSOR_BUFFER_SIZE: usize = 4096;
-/// Compression levels go from 0 to 11, where 11 has the highest compression
-/// ratio but requires more time.
-pub const BROTLI_COMPRESSOR_QUALITY: u32 = 11;
-/// The LZ77 window size (0, 10-24) where bigger windows size improves density.
-pub const BROTLI_COMPRESSOR_LG_WINDOW_SIZE: u32 = 22;
 pub const OBJECT_SNAPSHOT_FILE_PATH: &str = "stardust_object_snapshot.bin";
 pub const IOTA_OBJECT_SNAPSHOT_URL: &str = "https://stardust-objects.s3.eu-central-1.amazonaws.com/iota/alphanet/latest/stardust_object_snapshot.bin.gz";
 pub const SHIMMER_OBJECT_SNAPSHOT_URL: &str = "https://stardust-objects.s3.eu-central-1.amazonaws.com/shimmer/alphanet/latest/stardust_object_snapshot.bin.gz";
@@ -1447,8 +1441,6 @@ pub fn split_timelocks(
 pub enum SnapshotSource {
     /// Local uncompressed file.
     Local(PathBuf),
-    /// Local file compressed with brotli.
-    LocalBrotli(PathBuf),
     /// Remote file (S3) with gzip compressed file
     S3(SnapshotUrl),
 }
@@ -1459,10 +1451,6 @@ impl SnapshotSource {
         Ok(match self {
             SnapshotSource::Local(path) => Box::new(BufReader::new(File::open(path)?)),
             SnapshotSource::S3(snapshot_url) => Box::new(snapshot_url.to_reader()?),
-            SnapshotSource::LocalBrotli(path) => Box::new(brotli::Decompressor::new(
-                File::open(path)?,
-                BROTLI_COMPRESSOR_BUFFER_SIZE,
-            )),
         })
     }
 }
