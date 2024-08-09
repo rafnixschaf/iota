86c8e3d6ff3f561ddcf79830ec00ff6ec9dc8c3e
Valerii Reutov <valeriy.reutov@gmail.com>
2024-08-09 10:55:09 +0300

    feat(docs): Using an Alias object test scenario (#1679)
    * Add example ofr third party simple nft package, PTB that create custom nft from stardust::nft
    
    * feat(examples/docs): Add a conversion function for custom NFTs to allow migrating custom NFTs from Stardust NFTs
    
    * Add function that publishes random nft package via CLI.
    
    * feat(examples): replace package publishing approach with iota-move-builder
    
    * Fix chkecs
    
    * Minor refactoring
    
    * dprint fix
    
    * Fix review comments
    
    * feat(docs/content): add doc related to conversion of a NFT Output into sutom NFT
    
    * feat(docs): extended the custom_nft package with collections
    
    * Review comment fixes
    
    * Fix Nft naming
    
    * fix(docs): NFTs conversion docs were fixed after refactoring
    
    * fix(docs): spelling issues
    
    ---------
    
    Co-authored-by: Dkwcs <pavlo.botnar@gmail.com>

--- docs/examples/rust/stardust/alias-migration.rs
+++ docs/examples/rust/stardust/alias-migration.rs
@@ -1,18 +1,20 @@
 // Copyright (c) 2024 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-//! Example demonstrating the conversion of a stardust NFT into a custom user's
-//! NFT. In order to work, it requires a network with test objects
-//! generated from iota-genesis-builder/src/stardust/test_outputs.
+//! Example demonstrating the conversion of a stardust Alias into a custom
+//! user's NFT collections controller. In order to work, it requires a network
+//! with test objects generated from
+//! iota-genesis-builder/src/stardust/test_outputs.
 
-use std::{fs, path::PathBuf};
+use std::{fs, path::PathBuf, str::FromStr};
 
 use anyhow::{anyhow, Result};
 use iota_keys::keystore::{AccountKeystore, FileBasedKeystore};
 use iota_move_build::BuildConfig;
 use iota_sdk::{
     rpc_types::{
-        IotaObjectDataOptions, IotaTransactionBlockEffectsAPI, IotaTransactionBlockResponseOptions,
+        IotaData, IotaObjectDataOptions, IotaTransactionBlockEffectsAPI,
+        IotaTransactionBlockResponseOptions,
     },
     types::{
         base_types::{IotaAddress, ObjectID},
@@ -20,8 +22,9 @@ use iota_sdk::{
         gas_coin::GAS,
         programmable_transaction_builder::ProgrammableTransactionBuilder,
         quorum_driver_types::ExecuteTransactionRequestType,
-        transaction::{Argument, ObjectArg, Transaction, TransactionData},
-        IOTA_FRAMEWORK_ADDRESS, STARDUST_ADDRESS,
+        stardust::output::AliasOutput,
+        transaction::{Argument, CallArg, ObjectArg, Transaction, TransactionData},
+        TypeTag, IOTA_FRAMEWORK_PACKAGE_ID, STARDUST_PACKAGE_ID,
     },
     IotaClient, IotaClientBuilder,
 };
@@ -60,81 +63,184 @@ async fn main() -> Result<(), anyhow::Error> {
         .next()
         .ok_or(anyhow!("No coins found"))?;
 
-    // Get an NftOutput object id
-    let nft_output_object_id = ObjectID::from_hex_literal(
-        "0x6445847625cec7d1265ebb9d0da8050a2e43d2856c2746d3579df499a1a64226",
+    // Get an AliasOutput object id
+    let alias_output_object_id = ObjectID::from_hex_literal(
+        "0x354a1864c8af23fde393f7603bc133f755a9405353b30878e41b929eb7e37554",
     )?;
 
-    // Get an NftOutput object
-    let nft_output_object = iota_client
+    // Get an AliasOutput object
+    let alias_output_object = iota_client
         .read_api()
         .get_object_with_options(
-            nft_output_object_id,
+            alias_output_object_id,
             IotaObjectDataOptions::new().with_bcs(),
         )
         .await?
         .data
-        .ok_or(anyhow!("Nft output not found"))?;
+        .ok_or(anyhow!("Alias output not found"))?;
+    let alias_output_object_ref = alias_output_object.object_ref();
 
-    let nft_output_object_ref = nft_output_object.object_ref();
+    // Convert the AliasOutput object into its Rust representation.
+    let alias_output = bcs::from_bytes::<AliasOutput>(
+        &alias_output_object
+            .bcs
+            .expect("should contain bcs")
+            .try_as_move()
+            .expect("should convert it to a move object")
+            .bcs_bytes,
+    )?;
 
-    // Create a PTB that extracts the stardust NFT from an NFTOutput and then calls
-    // the `custom_nft::nft::convert` function for converting it into a custom NFT
-    // of the just published package.
+    // Extract the keys of the native_tokens bag if it is not empty; the keys
+    // are the type_arg of each native token, so they can be used later in the PTB.
+    let mut df_type_keys: Vec<String> = vec![];
+    let native_token_bag = alias_output.native_tokens;
+    if native_token_bag.size > 0 {
+        // Get the dynamic fields owned by the native tokens bag.
+        let dynamic_field_page = iota_client
+            .read_api()
+            .get_dynamic_fields(*native_token_bag.id.object_id(), None, None)
+            .await?;
+        // Only one page should exist.
+        assert!(!dynamic_field_page.has_next_page);
+
+        // Extract the dynamic fields keys, i.e., the native token type.
+        df_type_keys.extend(
+            dynamic_field_page
+                .data
+                .into_iter()
+                .map(|dyi| {
+                    dyi.name
+                        .value
+                        .as_str()
+                        .expect("should be a string")
+                        .to_string()
+                })
+                .collect::<Vec<_>>(),
+        );
+    }
+
+    // Create a PTB that extracts the related stardust Alias from the AliasOutput
+    // and then calls the
+    // `custom_nft::collection::convert_alias_to_collection_controller_cap` function
+    // to convert it into an NFT collection controller, create a collection and mint
+    // a few NFTs.
     let pt = {
         let mut builder = ProgrammableTransactionBuilder::new();
-        let type_arguments = vec![GAS::type_tag()];
-        let arguments = vec![builder.obj(ObjectArg::ImmOrOwnedObject(nft_output_object_ref))?];
+
+        let arguments = vec![builder.obj(ObjectArg::ImmOrOwnedObject(alias_output_object_ref))?];
         // Call the nft_output::extract_assets function
         if let Argument::Result(extracted_assets) = builder.programmable_move_call(
-            STARDUST_ADDRESS.into(),
-            ident_str!("nft_output").to_owned(),
+            STARDUST_PACKAGE_ID,
+            ident_str!("alias_output").to_owned(),
             ident_str!("extract_assets").to_owned(),
-            type_arguments,
+            vec![GAS::type_tag()],
             arguments,
         ) {
-            // If the nft output can be unlocked, the command will be successful
-            // and will return a `base_token` (i.e., IOTA) balance and a
-            // `Bag` of native tokens and related nft object.
+            // The alias output can always be unlocked by the governor address. So the
+            // command will be successful and will return a `base_token` (i.e., IOTA)
+            // balance, a `Bag` of the related native tokens and the related Alias object.
             let extracted_base_token = Argument::NestedResult(extracted_assets, 0);
-            let extracted_native_tokens_bag = Argument::NestedResult(extracted_assets, 1);
-            let nft_asset = Argument::NestedResult(extracted_assets, 2);
+            let mut extracted_native_tokens_bag = Argument::NestedResult(extracted_assets, 1);
+            let alias_asset = Argument::NestedResult(extracted_assets, 2);
 
-            // Call the conversion function to create a custom nft from the stardust nft
-            // asset.
-            let custom_nft = builder.programmable_move_call(
+            // Call the conversion function to create an NFT collection controller from the
+            // extracted alias.
+            let nft_collection_controller = builder.programmable_move_call(
                 custom_nft_package_id,
-                ident_str!("nft").to_owned(),
-                ident_str!("convert").to_owned(),
+                ident_str!("collection").to_owned(),
+                ident_str!("convert_alias_to_collection_controller_cap").to_owned(),
                 vec![],
-                vec![nft_asset],
+                vec![alias_asset],
             );
 
-            // Transfer the converted NFT
-            builder.transfer_arg(sender, custom_nft);
+            // Create an NFT collection
+            let nft_collection_name = builder
+                .input(CallArg::Pure(bcs::to_bytes("Collection name").unwrap()))
+                .unwrap();
+
+            let nft_collection = builder.programmable_move_call(
+                custom_nft_package_id,
+                ident_str!("collection").to_owned(),
+                ident_str!("create_collection").to_owned(),
+                vec![],
+                vec![nft_collection_controller, nft_collection_name],
+            );
+
+            // Mint a collection-related NFT
+            let nft_name = builder
+                .input(CallArg::Pure(bcs::to_bytes("NFT name").unwrap()))
+                .unwrap();
+            let nft_description = builder
+                .input(CallArg::Pure(bcs::to_bytes("NFT description").unwrap()))
+                .unwrap();
+            let nft_url_value = builder
+                .input(CallArg::Pure(bcs::to_bytes("NFT URL").unwrap()))
+                .unwrap();
+            let nft_url = builder.programmable_move_call(
+                IOTA_FRAMEWORK_PACKAGE_ID,
+                ident_str!("url").to_owned(),
+                ident_str!("new_unsafe").to_owned(),
+                vec![],
+                vec![nft_url_value],
+            );
+
+            let nft = builder.programmable_move_call(
+                custom_nft_package_id,
+                ident_str!("nft").to_owned(),
+                ident_str!("mint_collection_related").to_owned(),
+                vec![],
+                vec![nft_collection, nft_name, nft_description, nft_url],
+            );
+
+            // Transfer the NFT
+            builder.transfer_arg(sender, nft);
+
+            // Drop the NFT collection to make impossible to mint new related NFTs
+            builder.programmable_move_call(
+                custom_nft_package_id,
+                ident_str!("collection").to_owned(),
+                ident_str!("drop_collection").to_owned(),
+                vec![],
+                vec![nft_collection_controller, nft_collection],
+            );
+
+            // Transfer the NFT collection controller
+            builder.transfer_arg(sender, nft_collection_controller);
 
             // Extract IOTA balance
-            let arguments = vec![extracted_base_token];
-            let type_arguments = vec![GAS::type_tag()];
             let iota_coin = builder.programmable_move_call(
-                IOTA_FRAMEWORK_ADDRESS.into(),
+                IOTA_FRAMEWORK_PACKAGE_ID,
                 ident_str!("coin").to_owned(),
                 ident_str!("from_balance").to_owned(),
-                type_arguments,
-                arguments,
+                vec![GAS::type_tag()],
+                vec![extracted_base_token],
             );
 
             // Transfer IOTA balance
             builder.transfer_arg(sender, iota_coin);
 
+            // Extract the native tokens from the bag.
+            for type_key in df_type_keys {
+                let type_arguments = vec![TypeTag::from_str(&format!("0x{type_key}"))?];
+                let arguments = vec![extracted_native_tokens_bag, builder.pure(sender)?];
+
+                // Extract a native token balance.
+                extracted_native_tokens_bag = builder.programmable_move_call(
+                    STARDUST_PACKAGE_ID,
+                    ident_str!("utilities").to_owned(),
+                    ident_str!("extract_and_send_to").to_owned(),
+                    type_arguments,
+                    arguments,
+                );
+            }
+
             // Cleanup bag.
-            let arguments = vec![extracted_native_tokens_bag];
             builder.programmable_move_call(
-                IOTA_FRAMEWORK_ADDRESS.into(),
+                IOTA_FRAMEWORK_PACKAGE_ID,
                 ident_str!("bag").to_owned(),
                 ident_str!("destroy_empty").to_owned(),
                 vec![],
-                arguments,
+                vec![extracted_native_tokens_bag],
             );
         }
         builder.finish()
