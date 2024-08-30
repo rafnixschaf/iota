8b7d408af57efc060447eef4c83619cb648278ce
evavirseda <evirseda@boxfish.studio>
2024-08-30 13:06:47 +0200

    feat(wallet): rebrand recover many page (#2094)
    * feat: update account component
    
    * feat: rebrand recovery many page
    
    * feat: refine icons
    
    * feat: fix overflow

--- apps/wallet/src/ui/app/components/DAppInfoCard.tsx
+++ apps/wallet/src/ui/app/components/DAppInfoCard.tsx
@@ -4,15 +4,11 @@
 
 import { type PermissionType } from '_src/shared/messaging/messages/payloads/permissions';
 import { getValidDAppUrl } from '_src/shared/utils';
-import { CheckFill16 } from '@iota/icons';
-import cn from 'clsx';
-
 import { useAccountByAddress } from '../hooks/useAccountByAddress';
 import { Heading } from '../shared/heading';
 import { Link } from '../shared/Link';
 import { AccountIcon } from './accounts/AccountIcon';
 import { AccountItem } from './accounts/AccountItem';
-import { LockUnlockButton } from './accounts/LockUnlockButton';
 import { useUnlockAccount } from './accounts/UnlockAccountContext';
 import { DAppPermissionsList } from './DAppPermissionsList';
 import { SummaryCard } from './SummaryCard';
@@ -35,8 +31,15 @@ export function DAppInfoCard({
     const validDAppUrl = getValidDAppUrl(url);
     const appHostname = validDAppUrl?.hostname ?? url;
     const { data: account } = useAccountByAddress(connectedAddress);
-    const { unlockAccount, lockAccount, isPending, accountToUnlock } = useUnlockAccount();
-
+    const { unlockAccount, lockAccount } = useUnlockAccount();
+    function handleLockAndUnlockClick() {
+        if (!account) return;
+        if (account?.isLocked) {
+            unlockAccount(account);
+        } else {
+            lockAccount(account);
+        }
+    }
     return (
         <div className="flex flex-col gap-5 bg-white p-6">
             <div className="flex flex-row flex-nowrap items-center gap-3.75 py-3">
@@ -64,34 +67,8 @@ export function DAppInfoCard({
                 <AccountItem
                     icon={<AccountIcon account={account} />}
                     accountID={account.id}
-                    disabled={account.isLocked}
-                    after={
-                        <div className="flex flex-1 items-center justify-end gap-1">
-                            {account.isLocked ? (
-                                <div className="h-4">
-                                    <LockUnlockButton
-                                        isLocked={account.isLocked}
-                                        isLoading={isPending && accountToUnlock?.id === account.id}
-                                        onClick={(e) => {
-                                            // prevent the account from being selected when clicking the lock button
-                                            e.stopPropagation();
-                                            if (account.isLocked) {
-                                                unlockAccount(account);
-                                            } else {
-                                                lockAccount(account);
-                                            }
-                                        }}
-                                    />
-                                </div>
-                            ) : null}
-                            <CheckFill16
-                                className={cn(
-                                    'h-4 w-4',
-                                    account.isLocked ? 'text-hero/10' : 'text-success',
-                                )}
-                            />
-                        </div>
-                    }
+                    onLockAccountClick={handleLockAndUnlockClick}
+                    onUnlockAccountClick={handleLockAndUnlockClick}
                     hideCopy
                     hideExplorerLink
                 />
