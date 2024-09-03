a9e2da87b31b5a748da95317a3be1d28cf3f98cd
Lucas Tortora <85233773+lucas-tortora@users.noreply.github.com>
2024-09-03 18:13:44 -0300

    feat(devx): Edit Developer > Stardust docs (#2063)
    * feat(devx): edit stardust docs
    
    * feat(devx): archive stardust doc stubs
    
    * feat(devx): archive stardust doc stubs
    
    * feat(devx): add warning
    
    * fix(devx): fix sidebar after merge
    
    * feat(devx): change tabs groupId from code-language to language
    
    * Update docs/content/developer/stardust/claiming/self-sponsor.mdx
    
    Co-authored-by: Dr-Electron <dr-electr0n@protonmail.com>
    
    ---------
    
    Co-authored-by: Dr-Electron <dr-electr0n@protonmail.com>

--- docs/content/sidebars/developer.js
+++ docs/content/sidebars/developer.js
@@ -317,11 +317,6 @@ const developer = [
                     },
                 ],
             },
-            'developer/stardust/vested',
-            'developer/stardust/testing',
-            'developer/stardust/if-tools',
-            'developer/stardust/faq',
-            'developer/stardust/advanced',
         ],
     },
     {
