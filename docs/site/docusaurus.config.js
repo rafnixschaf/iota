cdb9746247b9bd298d782bedfaed51c0948cfe45
Dr-Electron <dr-electr0n@protonmail.com>
2024-09-10 16:08:47 +0200

    feat(docs): update identity refs and re-enable broken anchor check (#2225)
    Co-authored-by: Eike Haß <eike-hass@web.de>

--- docs/site/docusaurus.config.js
+++ docs/site/docusaurus.config.js
@@ -28,7 +28,7 @@ const config = {
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "throw",
-  onBrokenAnchors: "warn",
+  onBrokenAnchors: "throw",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
