e6656e1ef1c6ac56b450600470597945573c9f5c
Bran <52735957+brancoder@users.noreply.github.com>
2024-09-16 15:50:25 +0200

    feat: rebrand tx approval request (#2560)
    * feat: rework Validator Logo
    
    * fix: update stake amount and key value info on stak tx card
    
    * feat: rework TxnAmount component
    
    * feat: rework GasSummary and add StakeTxnInfo panel
    
    * fix: remove unused components
    
    * feat: adjust stlying of stakeTxCard
    
    * fix: styling of the unstake transaction
    
    * fix: rename  receipt components
    
    * fix: add panel to unstakeTx component
    
    * feat: update transaction status component
    
    * fix: rebrand Balance Changes component
    
    * fix: rebrand Object Changes component
    
    * fix: resolve comments
    
    * fix: add full width to stake/unstake panel
    
    * fix: rebrand ObjectChangeDisplay component
    
    * fix: Delegation Details styling
    
    * fix: revert number hash in Epoch string
    
    * fix: remove stale comment
    
    * fix: typo
    
    * fix: rebrand gas fees component
    
    * fix: add missing keys to the for loops in templates
    
    * fix: rework gas component
    
    * fix: show gas only if sent transaction recepit
    
    * fix: rebrand show more/less button in expandable list
    
    * fix: remove unused components
    
    * feat: rebrand tx approval request

--- apps/wallet/src/ui/app/pages/approval-request/transaction-request/TransactionDetails/index.tsx
+++ apps/wallet/src/ui/app/pages/approval-request/transaction-request/TransactionDetails/index.tsx
@@ -3,65 +3,121 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { useTransactionData } from '_src/ui/app/hooks';
-import { Tab as HeadlessTab, type TabProps } from '@headlessui/react';
 import { type TransactionBlock } from '@iota/iota-sdk/transactions';
 
-import { SummaryCard } from '../SummaryCard';
 import { Command } from './Command';
 import { Input } from './Input';
+import { Collapsible } from '_src/ui/app/shared/collapse';
+import {
+    ButtonSegment,
+    ButtonSegmentType,
+    Panel,
+    SegmentedButton,
+    SegmentedButtonType,
+    Title,
+    TitleSize,
+} from '@iota/apps-ui-kit';
+import { useEffect, useState } from 'react';
+import { Alert, Loading } from '_src/ui/app/components';
 
 interface TransactionDetailsProps {
     sender?: string;
     transaction: TransactionBlock;
 }
 
-const Tab = (props: TabProps<'div'>) => (
-    <HeadlessTab
-        className="text-steel-darker ui-selected:border-hero ui-selected:text-hero-dark -mb-px cursor-pointer border-0 border-b border-solid border-transparent bg-transparent p-0 pb-2 text-body font-semibold outline-none"
-        {...props}
-    />
-);
+enum DetailsCategory {
+    Transactions = 'Transactions',
+    Inputs = 'Inputs',
+}
+const DETAILS_CATEGORIES = [
+    {
+        label: 'Transactions',
+        value: DetailsCategory.Transactions,
+    },
+    {
+        label: 'Inputs',
+        value: DetailsCategory.Inputs,
+    },
+];
 
 export function TransactionDetails({ sender, transaction }: TransactionDetailsProps) {
+    const [selectedDetailsCategory, setSelectedDetailsCategory] = useState<DetailsCategory | null>(
+        null,
+    );
     const { data: transactionData, isPending, isError } = useTransactionData(sender, transaction);
+    useEffect(() => {
+        if (transactionData) {
+            const defaultCategory =
+                transactionData.transactions.length > 0
+                    ? DetailsCategory.Transactions
+                    : transactionData.inputs.length > 0
+                      ? DetailsCategory.Inputs
+                      : null;
+
+            if (defaultCategory) {
+                setSelectedDetailsCategory(defaultCategory);
+            }
+        }
+    }, [transactionData]);
+
     if (transactionData?.transactions.length === 0 && transactionData.inputs.length === 0) {
         return null;
     }
     return (
-        <SummaryCard header="Transaction Details" initialExpanded>
-            {isPending || isError ? (
-                <div className="text-steel-darker ml-0 text-pBodySmall font-medium">
-                    {isPending ? 'Gathering data...' : "Couldn't gather data"}
-                </div>
-            ) : transactionData ? (
-                <div>
-                    <HeadlessTab.Group>
-                        <HeadlessTab.List className="border-gray-45 mb-6 flex gap-6 border-0 border-b border-solid">
-                            {!!transactionData.transactions.length && <Tab>Transactions</Tab>}
-                            {!!transactionData.inputs.length && <Tab>Inputs</Tab>}
-                        </HeadlessTab.List>
-                        <HeadlessTab.Panels>
-                            {!!transactionData.transactions.length && (
-                                <HeadlessTab.Panel className="flex flex-col gap-6">
-                                    {/* TODO: Rename components: */}
-                                    {transactionData.transactions.map((command, index) => (
-                                        <Command key={index} command={command} />
-                                    ))}
-                                </HeadlessTab.Panel>
-                            )}
-                            {!!transactionData.inputs.length && (
-                                <HeadlessTab.Panel className="flex flex-col gap-2">
-                                    {transactionData.inputs.map((input, index) => (
-                                        <Input key={index} input={input} />
-                                    ))}
-                                </HeadlessTab.Panel>
-                            )}
-                        </HeadlessTab.Panels>
-                    </HeadlessTab.Group>
-                </div>
-            ) : (
-                ''
-            )}
-        </SummaryCard>
+        <Panel hasBorder>
+            <div className="flex flex-col gap-y-sm overflow-hidden rounded-xl">
+                <Collapsible
+                    hideBorder
+                    defaultOpen
+                    render={() => <Title size={TitleSize.Small} title="Transaction Details" />}
+                >
+                    <SegmentedButton type={SegmentedButtonType.Transparent}>
+                        {DETAILS_CATEGORIES.map(({ label, value }) => (
+                            <ButtonSegment
+                                type={ButtonSegmentType.Underlined}
+                                key={value}
+                                onClick={() => setSelectedDetailsCategory(value)}
+                                label={label}
+                                selected={selectedDetailsCategory === value}
+                                disabled={
+                                    DetailsCategory.Transactions === value
+                                        ? transactionData?.transactions.length === 0
+                                        : DetailsCategory.Inputs === value
+                                          ? transactionData?.inputs.length === 0
+                                          : false
+                                }
+                            />
+                        ))}
+                    </SegmentedButton>
+                    <Loading loading={isPending}>
+                        {isError ? (
+                            <Alert>
+                                <div>
+                                    <strong>Couldn't gather data</strong>
+                                </div>
+                            </Alert>
+                        ) : null}
+                        <div className="flex flex-col p-md">
+                            {selectedDetailsCategory === DetailsCategory.Transactions &&
+                                !!transactionData?.transactions.length && (
+                                    <div className="flex flex-col gap-md">
+                                        {transactionData?.transactions.map((command, index) => (
+                                            <Command key={index} command={command} />
+                                        ))}
+                                    </div>
+                                )}
+                            {selectedDetailsCategory === DetailsCategory.Inputs &&
+                                !!transactionData?.inputs.length && (
+                                    <div className="flex flex-col gap-md">
+                                        {transactionData?.inputs.map((input, index) => (
+                                            <Input key={index} input={input} />
+                                        ))}
+                                    </div>
+                                )}
+                        </div>
+                    </Loading>
+                </Collapsible>
+            </div>
+        </Panel>
     );
 }
