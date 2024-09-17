7ec92a29c8465078808f92435449af4da86e9301
Thibault Martinez <thibault@iota.org>
2024-09-17 18:16:15 +0200

    Add crates/iota-service (#2665)
    Co-authored-by: Alexander Sporn <github@alexsporn.de>

---
+++ crates/iota-service/src/service.rs
@@ -0,0 +1,31 @@
+// Copyright (c) Mysten Labs, Inc.
+// Modifications Copyright (c) 2024 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+use anyhow::Result;
+use axum::{routing::get, Json, Router};
+use tracing::debug;
+
+use crate::{health::HealthResponse, DEFAULT_PORT};
+
+pub fn get_iota_service<S>(app_name: &str, app_version: &str) -> Router<S>
+where
+    S: Send + Clone + Sync + 'static,
+{
+    // build our application with a single route
+    Router::new().route(
+        "/health",
+        get(Json(HealthResponse::new(app_name, app_version))),
+    )
+}
+
+pub async fn serve(app: Router) -> Result<()> {
+    // run it with hyper on localhost:3000
+    debug!("listening on http://localhost:{}", DEFAULT_PORT);
+
+    let listener = tokio::net::TcpListener::bind(&format!("0.0.0.0:{}", DEFAULT_PORT))
+        .await
+        .unwrap();
+    axum::serve(listener, app).await?;
+    Ok(())
+}
