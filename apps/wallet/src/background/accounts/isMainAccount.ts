a810d8b53178e0a365800a752d9c397c86e2e797
Eugene P. <panteleymonchuk@gmail.com>
2024-09-25 10:41:00 +0300

    fix(wallet): update isMainAccount function. (#2814)
    Signed-off-by: Eugene Panteleymonchuk <panteleymonchuk@gmail.com>

--- apps/wallet/src/background/accounts/isMainAccount.ts
+++ apps/wallet/src/background/accounts/isMainAccount.ts
@@ -8,21 +8,19 @@ import { parseDerivationPath } from '_src/background/account-sources/bip44Path';
 import type { SerializedUIAccount } from '_src/background/accounts/Account';
 
 export function isMainAccount(account: SerializedUIAccount | null) {
-    {
-        if (!account) {
-            return false;
-        }
+    if (!account) {
+        return false;
+    }
 
-        if (
-            isLedgerAccountSerializedUI(account) ||
-            isMnemonicSerializedUiAccount(account) ||
-            isSeedSerializedUiAccount(account)
-        ) {
-            const { addressIndex, changeIndex, accountIndex } = parseDerivationPath(
-                account.derivationPath,
-            );
+    if (
+        isLedgerAccountSerializedUI(account) ||
+        isMnemonicSerializedUiAccount(account) ||
+        isSeedSerializedUiAccount(account)
+    ) {
+        const { addressIndex, changeIndex, accountIndex } = parseDerivationPath(
+            account.derivationPath,
+        );
 
-            return addressIndex === 0 && changeIndex === 0 && accountIndex === 0;
-        }
+        return addressIndex === 0 && changeIndex === 0 && accountIndex === 0;
     }
 }
