4a5fe8aa225c0e9f26d1e74dc7773714031f97a1
Begoña Álvarez de la Cruz <balvarez@boxfish.studio>
2024-09-26 17:26:00 +0200

    refactor(wallet): rebrand restricted view (#2897)

--- apps/wallet/src/ui/app/pages/restricted/index.tsx
+++ apps/wallet/src/ui/app/pages/restricted/index.tsx
@@ -2,20 +2,28 @@
 // Modifications Copyright (c) 2024 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
+import { IotaLogoWeb } from '@iota/ui-icons';
+import { PageMainLayout } from '_src/ui/app/shared/page-main-layout/PageMainLayout';
 import { useInitializedGuard } from '../../hooks';
-import { Text } from '../../shared/text';
-import SadCapy from './SadCapy.svg';
 
 export function RestrictedPage() {
     useInitializedGuard(true);
 
+    const CURRENT_YEAR = new Date().getFullYear();
+
     return (
-        <div className="bg-iota/10 flex w-full max-w-[400px] flex-col items-center gap-10 rounded-20 px-10 py-15 text-center">
-            <SadCapy role="presentation" />
-            <Text variant="pBody" color="steel-darker" weight="medium">
-                Regrettably this service is not available to you. Applicable laws prohibit us from
-                providing our services to your location at this time.
-            </Text>
-        </div>
+        <PageMainLayout>
+            <div className="flex h-full w-full flex-col items-center justify-between bg-white px-md py-2xl shadow-wallet-content">
+                <IotaLogoWeb width={130} height={32} />
+                <div className="flex flex-col items-center text-center">
+                    <span className="text-title-lg text-neutral-40">
+                        Regrettably this service is currently not available. Please try again later.
+                    </span>
+                </div>
+                <div className="text-body-lg text-neutral-60">
+                    &copy; IOTA Foundation {CURRENT_YEAR}
+                </div>
+            </div>
+        </PageMainLayout>
     );
 }
