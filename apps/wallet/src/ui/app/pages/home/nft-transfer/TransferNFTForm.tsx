15121c825aaf7196cc6018042f7b0969d482aae0
Bran <52735957+brancoder@users.noreply.github.com>
2024-09-26 14:52:59 +0200

    fix(wallet): Improve send token and nft forms (#2778)
    * fix: nft token send form initial validation
    
    * fix: manually validate form inputs in Send Token Form
    
    * fix: disbling button in TransferNftForm

--- apps/wallet/src/ui/app/pages/home/nft-transfer/TransferNFTForm.tsx
+++ apps/wallet/src/ui/app/pages/home/nft-transfer/TransferNFTForm.tsx
@@ -88,11 +88,11 @@ export function TransferNFTForm({ objectId, objectType }: TransferNFTFormProps) 
             initialValues={{
                 to: '',
             }}
-            validateOnMount
+            validateOnChange
             validationSchema={validationSchema}
             onSubmit={({ to }) => transferNFT.mutateAsync(to)}
         >
-            {({ isValid }) => (
+            {({ isValid, dirty }) => (
                 <Form autoComplete="off" className="h-full">
                     <div className="flex h-full flex-col justify-between">
                         <Field
@@ -102,7 +102,11 @@ export function TransferNFTForm({ objectId, objectType }: TransferNFTFormProps) 
                             placeholder="Enter Address"
                         />
 
-                        <Button htmlType={ButtonHtmlType.Submit} disabled={!isValid} text="Send" />
+                        <Button
+                            htmlType={ButtonHtmlType.Submit}
+                            disabled={!(isValid && dirty)}
+                            text="Send"
+                        />
                     </div>
                 </Form>
             )}
