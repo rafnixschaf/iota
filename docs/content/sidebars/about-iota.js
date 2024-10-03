40f2b7a8067b60bc1d4562a57e10e35ceac3b913
Lucas Tortora <85233773+lucas-tortora@users.noreply.github.com>
2024-10-03 11:02:59 -0300

    feat(docs)  - Add Wallet Docs (#2875)
    * feat(devx): Added base docs for the IOTA wallet
    
    * feat(devx): Added base docs for the IOTA wallet
    
    * feat(devx): Edit getting started and add pics
    
    * feat(devx): Add pics to how to guide
    
    * feat(devx): edit how to guides
    
    * feat(devx): edit staking and add pics
    
    * feat(devx): edit multi-account and add pics
    
    * feat(devx): edit FAQ
    
    * feat(devx): edit test and add pics
    
    * feat(devx): edit test
    
    * feat(devx): small edits
    
    * feat(devx): small edits
    
    * feat(devx): add link
    
    * feat(devx): edit ledger integration doc and add missing pics to stake
    
    * feat(devx): edit ledger integration doc and add missing pics to stake
    
    * feat(devx): edits
    
    * fix(devx): dead link
    
    * feat(devx): add wallet link
    
    * feat(devx): remove TODP
    
    * feat(devx): incorporte feedback
    
    * feat(devx): add todo

--- docs/content/sidebars/about-iota.js
+++ docs/content/sidebars/about-iota.js
@@ -7,7 +7,6 @@ const aboutIota = [
     {
         type: 'category',
         label: 'IOTA Architecture',
-        collapsed: false,
         link: {
             type: 'doc',
             id: 'about-iota/iota-architecture/iota-architecture',
@@ -24,7 +23,6 @@ const aboutIota = [
     {
         type: 'category',
         label: 'Tokenomics',
-        collapsed: false,
         link: {
             type: 'doc',
             id: 'about-iota/tokenomics/tokenomics',
@@ -37,6 +35,25 @@ const aboutIota = [
             'about-iota/tokenomics/staking-unstaking',
             'about-iota/tokenomics/gas-in-iota',
             'about-iota/tokenomics/gas-pricing',
+        ],
+    },
+    {
+        type: 'category',
+        label: 'IOTA Wallet',
+        items: [
+            'about-iota/iota-wallet/getting-started',
+            {
+                type:'category',
+                label:'How To',
+                items:[
+                     'about-iota/iota-wallet/how-to/basics',
+                     'about-iota/iota-wallet/how-to/stake',
+                     'about-iota/iota-wallet/how-to/multi-account',
+                    'about-iota/iota-wallet/how-to/get-test-tokens',
+                     'about-iota/iota-wallet/how-to/integrate-ledger',
+                ]
+            },
+            'about-iota/iota-wallet/FAQ',
         ],
     },
 ];
