4ea9a6a7f46acf0c22feb36cecb23f1b79c80882
Levente Pap <levente.pap@iota.org>
2024-10-16 15:16:01 +0200

    fix(iota-source-validation-service): test_api_route works again (#3324)

--- crates/iota-source-validation-service/tests/tests.rs
+++ crates/iota-source-validation-service/tests/tests.rs
@@ -308,7 +308,7 @@ async fn test_api_route() -> anyhow::Result<()> {
         sources_list,
     }));
 
-    serve(app_state).await.expect("Cannot start service");
+    tokio::spawn(async move { serve(app_state).await.expect("Cannot start service.") });
     tokio::time::sleep(std::time::Duration::from_secs(1)).await;
 
     let client = Client::new();
@@ -362,7 +362,7 @@ async fn test_api_route() -> anyhow::Result<()> {
 #[tokio::test]
 async fn test_metrics_route() -> anyhow::Result<()> {
     // Start metrics server
-    let metrics_listener = tokio::net::TcpListener::bind(METRICS_HOST_PORT).await?;
+    let metrics_listener = std::net::TcpListener::bind(METRICS_HOST_PORT)?;
     let registry_service = start_prometheus_server(metrics_listener);
     let prometheus_registry = registry_service.default_registry();
     SourceServiceMetrics::new(&prometheus_registry);
