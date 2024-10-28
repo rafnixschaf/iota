a8d14aaf4b5ba1500e497e3c11a2b658f1cf5912
evavirseda <evirseda@boxfish.studio>
2024-10-28 19:00:24 +0100

    feat(apps-ui-kit): add support for right aligned dialog (#3653)
    Co-authored-by: JCNoguera <88061365+VmMad@users.noreply.github.com>
    Co-authored-by: Begoña Álvarez de la Cruz <balvarez@boxfish.studio>

--- apps/ui-kit/src/storybook/stories/organisms/Dialog.stories.tsx
+++ apps/ui-kit/src/storybook/stories/organisms/Dialog.stories.tsx
@@ -1,6 +1,6 @@
 // Copyright (c) 2024 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
-import { ButtonSize } from '@/lib/components';
+import { ButtonSize, DialogPosition } from '@/lib/components';
 
 import type { Meta, StoryObj } from '@storybook/react';
 
@@ -54,3 +54,50 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {};
+
+export const RightDialog: Story = {
+    render: () => {
+        const [open, setOpen] = useState(false);
+        return (
+            <div className="flex">
+                <Button size={ButtonSize.Small} text="Open Dialog" onClick={() => setOpen(true)} />
+                <Dialog open={open} onOpenChange={setOpen}>
+                    <DialogContent showCloseOnOverlay position={DialogPosition.Right}>
+                        <div className="h-full w-full">
+                            <Header
+                                title="Connect Ledger Wallet"
+                                titleCentered
+                                onClose={() => setOpen(false)}
+                                onBack={() => setOpen(false)}
+                            />
+                            <div className="flex h-full w-full flex-col items-center justify-center">
+                                <DialogBody>
+                                    <div className="flex flex-col items-center gap-2">
+                                        <div className="mt-4.5">Logo</div>
+                                        <div className="mt-4.5 break-words text-center">
+                                            Connect your ledger to your computer, unlock it, and
+                                            launch the IOTA app. Click Continue when done.
+                                        </div>
+                                        <div className="mt-4.5">
+                                            {' '}
+                                            Need more help? View tutorial.
+                                        </div>
+                                    </div>
+                                </DialogBody>
+                                <div className="flex w-full flex-row justify-center gap-2 px-md--rs pb-md--rs pt-sm--rs">
+                                    <Button
+                                        size={ButtonSize.Small}
+                                        type={ButtonType.Outlined}
+                                        text="Cancel"
+                                        onClick={() => setOpen(false)}
+                                    />
+                                    <Button size={ButtonSize.Small} text="Connect" />
+                                </div>
+                            </div>
+                        </div>
+                    </DialogContent>
+                </Dialog>
+            </div>
+        );
+    },
+};
