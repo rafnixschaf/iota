a674cda26d2aef0b97d77347628ba80d421aba23
Dr-Electron <dr-electr0n@protonmail.com>
2024-10-29 10:48:00 +0100

    fix(docs): fix connect button style

--- docs/site/src/components/WalletConnectButton/index.tsx
+++ docs/site/src/components/WalletConnectButton/index.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import { ConnectButton } from '@iota/dapp-kit';
 
 export default function WalletConnectButton(): JSX.Element  {
-    return <ConnectButton />;
+    return <ConnectButton style={{ height: '2.625rem', marginRight: '0.75rem'}} />;
 }
