716dd4c2907dfc3970df882ee07758ecef60217c
Alexander Schmidt <alexander.schmidt@iota.org>
2024-10-30 13:55:40 +0100

    try release build

--- scripts/simtest/codecov.sh
+++ scripts/simtest/codecov.sh
@@ -18,7 +18,7 @@ export SIMTEST_STATIC_INIT_MOVE=$root_dir"/examples/move/basics"
 
 cargo llvm-cov clean
 
-RUSTFLAGS="-C debuginfo=0" MSIM_WATCHDOG_TIMEOUT_MS=60000 MSIM_TEST_SEED=1 cargo llvm-cov --ignore-run-fail --html nextest --cargo-profile simulator
+RUSTFLAGS="-C debuginfo=0" MSIM_WATCHDOG_TIMEOUT_MS=60000 MSIM_TEST_SEED=1 cargo llvm-cov --release --ignore-run-fail --html nextest --cargo-profile simulator
 
 # remove the patch
 git checkout .cargo/config Cargo.toml Cargo.lock
