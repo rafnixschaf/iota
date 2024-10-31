c88c6690c81519343ae48377a3b22fcc36d1107e
Alexander Schmidt <alexander.schmidt@iota.org>
2024-10-31 15:08:55 +0100

    clean up the mess

--- scripts/simtest/codecov.sh
+++ scripts/simtest/codecov.sh
@@ -16,14 +16,7 @@ git apply ./scripts/simtest/config-patch
 root_dir=$(git rev-parse --show-toplevel)
 export SIMTEST_STATIC_INIT_MOVE=$root_dir"/examples/move/basics"
 
-cargo llvm-cov clean
-
-MSIM_WATCHDOG_TIMEOUT_MS=60000 MSIM_TEST_SEED=1 cargo +nightly llvm-cov --ignore-run-fail --branch --html nextest \
-  --cargo-profile simulator \
-  --workspace \
-  --exclude iota-e2e-tests \
-  --exclude iota-json-rpc-tests \
-  --exclude iota-faucet
+MSIM_WATCHDOG_TIMEOUT_MS=60000 MSIM_TEST_SEED=1 cargo +nightly llvm-cov --ignore-run-fail --branch --html nextest --cargo-profile simulator
 
 # remove the patch
 git checkout .cargo/config Cargo.toml Cargo.lock
