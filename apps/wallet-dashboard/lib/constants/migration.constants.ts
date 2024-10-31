2ea4ee13fe5afa77e4d581b0f2e118d4e28bc742
Branko Bosnic <brankobosnic1@gmail.com>
2024-10-31 13:36:30 +0100

    fix: migration constants and variable nameing

--- apps/wallet-dashboard/lib/constants/migration.constants.ts
+++ apps/wallet-dashboard/lib/constants/migration.constants.ts
@@ -3,4 +3,4 @@
 export const STARDUST_PACKAGE_ID =
     '000000000000000000000000000000000000000000000000000000000000107a';
 export const STARDUST_BASIC_OUTPUT_TYPE = `${STARDUST_PACKAGE_ID}::basic_output::BasicOutput<0x2::iota::IOTA>`;
-export const STARDUST_NFT_OUTPUT_TYPE = `${STARDUST_PACKAGE_ID}::basic_output::BasicOutput<0x2::iota::IOTA>`;
+export const STARDUST_NFT_OUTPUT_TYPE = `${STARDUST_PACKAGE_ID}::nft_output::NftOutput<0x2::iota::IOTA>`;
