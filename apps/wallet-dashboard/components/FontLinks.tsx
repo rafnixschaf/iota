568247e5239aa9b736b7fec23cf87b76e3b6d755
JCNoguera <88061365+VmMad@users.noreply.github.com>
2024-11-06 15:28:08 +0100

    feat(wallet-dashboard): add missing link for `AllianceNo2` (#3825)
    * feat(wallet-dashboard): add missing link for AllianceNo2
    
    * fix: update names
    
    * fix: add missing import

---
+++ apps/wallet-dashboard/components/FontLinks.tsx
@@ -0,0 +1,8 @@
+// Copyright (c) 2024 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+const ALLIANCE_NO2_FONT_URL = 'https://webassets.iota.org/api/protected?face=alliance-no2';
+
+export function FontLinks() {
+    return <link rel="stylesheet" href={ALLIANCE_NO2_FONT_URL} />;
+}
