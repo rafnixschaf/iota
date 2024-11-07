7f2e14729353fb8d101241bae28248cbf872f922
Alexander Schmidt <alexander.schmidt@iota.org>
2024-11-07 15:35:10 +0100

    put option on the right command

--- scripts/simtest/simtest-cov.sh
+++ scripts/simtest/simtest-cov.sh
@@ -19,9 +19,14 @@ export SIMTEST_STATIC_INIT_MOVE=$root_dir"/examples/move/basics"
 TOOLCHAIN=$(rustup show active-toolchain | awk '{print $1}')
 LLVM_PROFDATA="$HOME/.rustup/toolchains/$TOOLCHAIN/lib/rustlib/x86_64-unknown-linux-gnu/bin/llvm-profdata"
 
-MSIM_WATCHDOG_TIMEOUT_MS=60000 MSIM_TEST_SEED=1 cargo llvm-cov --workspace --ignore-run-fail --no-report nextest -vv --cargo-profile simulator
+echo "Computing simtest code coverage."
+MSIM_WATCHDOG_TIMEOUT_MS=60000 MSIM_TEST_SEED=1 cargo llvm-cov \
+  --workspace \
+  --ignore-run-fail \
+  --no-report \
+  nextest -vv --cargo-profile simulator
 
-echo "Checking validity of generated .profraw files."
+echo "Checking validity of generated .profraw files. This might take a while."
 find target/llvm-cov-target -name '*.profraw' | while read file; do
   if ! "$LLVM_PROFDATA" show "$file" > /dev/null 2>&1; then
       echo "Removing corrupted file: $file"
@@ -30,7 +35,10 @@ find target/llvm-cov-target -name '*.profraw' | while read file; do
 done 
 
 echo "Creating simtest report."
-cargo llvm-cov report --lcov --output-path target/simtest.info
+cargo llvm-cov report \
+  --lcov \
+  --output-path target/simtest.info \
+  --ignore-filename-regex 'external-crates/.*'
 
 # remove the patch
 git checkout .cargo/config Cargo.toml Cargo.lock
