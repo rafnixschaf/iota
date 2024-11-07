bd0796378b7339e67eedcb62b7a000f727f465a0
Bran <52735957+brancoder@users.noreply.github.com>
2024-11-07 10:42:59 +0100

    feat(tooling-dashboard): organize migration stardust objects (#3826)
    * feat: fetch stardust basic and nft objects
    
    * feat: organize stardust objects on migratable and unmigratable
    
    * fix: migration constants and variable nameing
    
    * fix: variable naming
    
    * fix: move constants to core
    
    * fix: evert adding routes to barrel
    
    * fix: remove undefined object check
    
    ---------
    
    Co-authored-by: Marc Espin <mespinsanz@gmail.com>

---
+++ apps/wallet-dashboard/lib/utils/migration.ts
@@ -0,0 +1,51 @@
+// Copyright (c) 2024 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+import { IotaObjectData } from '@iota/iota-sdk/client';
+import { CommonOutputObjectWithUc } from '../interfaces/migration.interface';
+
+export type StardustMigrationGroupedObjects = {
+    migratable: IotaObjectData[];
+    unmigratable: IotaObjectData[];
+};
+
+export function groupStardustObjectsByMigrationStatus(
+    stardustOutputObjects: IotaObjectData[],
+    epochTimestamp: number,
+    address: string,
+): StardustMigrationGroupedObjects {
+    const migratable: IotaObjectData[] = [];
+    const unmigratable: IotaObjectData[] = [];
+
+    const epochUnix = epochTimestamp / 1000;
+
+    for (const outputObject of stardustOutputObjects) {
+        const outputObjectFields = (
+            outputObject.content as unknown as {
+                fields: CommonOutputObjectWithUc;
+            }
+        ).fields;
+
+        if (outputObjectFields.expiration_uc) {
+            const unlockableAddress =
+                outputObjectFields.expiration_uc.fields.unix_time <= epochUnix
+                    ? outputObjectFields.expiration_uc.fields.return_address
+                    : outputObjectFields.expiration_uc.fields.owner;
+            if (unlockableAddress !== address) {
+                unmigratable.push(outputObject);
+                continue;
+            }
+        }
+        if (
+            outputObjectFields.timelock_uc &&
+            outputObjectFields.timelock_uc.fields.unix_time > epochUnix
+        ) {
+            unmigratable.push(outputObject);
+            continue;
+        }
+
+        migratable.push(outputObject);
+    }
+
+    return { migratable, unmigratable };
+}
