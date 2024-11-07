c00dfa5cd8c9a6d4080df330a0e3790d99fd6e1f
Alexander Schmidt <alexander.schmidt@iota.org>
2024-11-07 10:55:04 +0100

    only code-coverage for workspace crates

--- scripts/simtest/simtest-cov.sh
+++ scripts/simtest/simtest-cov.sh
@@ -19,7 +19,7 @@ export SIMTEST_STATIC_INIT_MOVE=$root_dir"/examples/move/basics"
 TOOLCHAIN=$(rustup show active-toolchain | awk '{print $1}')
 LLVM_PROFDATA="$HOME/.rustup/toolchains/$TOOLCHAIN/lib/rustlib/x86_64-unknown-linux-gnu/bin/llvm-profdata"
 
-MSIM_WATCHDOG_TIMEOUT_MS=60000 MSIM_TEST_SEED=1 cargo llvm-cov --ignore-run-fail --no-report nextest -vv --cargo-profile simulator
+MSIM_WATCHDOG_TIMEOUT_MS=60000 MSIM_TEST_SEED=1 cargo llvm-cov --workspace --ignore-run-fail --no-report nextest -vv --cargo-profile simulator
 
 echo "Checking validity of generated .profraw files."
 find target/llvm-cov-target -name '*.profraw' | while read file; do
