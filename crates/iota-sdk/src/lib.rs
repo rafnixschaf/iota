70f3f1e0c70f7b294d2e4825b0158ed6e211916d
Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>
2024-11-11 15:27:45 +0100

    feat(iota): update testnet and devnet urls (#4001)
    * feat(iota): update testnet and devnet urls
    
    * fix, use v1/gas urls

--- crates/iota-sdk/src/lib.rs
+++ crates/iota-sdk/src/lib.rs
@@ -118,8 +118,8 @@ pub const IOTA_COIN_TYPE: &str = "0x2::iota::IOTA";
 pub const IOTA_LOCAL_NETWORK_URL: &str = "http://127.0.0.1:9000";
 pub const IOTA_LOCAL_NETWORK_URL_0: &str = "http://0.0.0.0:9000";
 pub const IOTA_LOCAL_NETWORK_GAS_URL: &str = "http://127.0.0.1:5003/gas";
-pub const IOTA_DEVNET_URL: &str = "https://fullnode.devnet.iota.io:443";
-pub const IOTA_TESTNET_URL: &str = "https://fullnode.testnet.iota.io:443";
+pub const IOTA_DEVNET_URL: &str = "https://api.devnet.iota.cafe";
+pub const IOTA_TESTNET_URL: &str = "https://api.testnet.iota.cafe";
 
 /// Builder for creating an [IotaClient] for connecting to the Iota network.
 ///
