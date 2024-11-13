f754f85ac201fecf6827d3ed736c8b355620fe6e
muXxer <git@muxxer.de>
2024-11-13 20:28:23 +0100

    rename(docker): rename `iota-source-service` to `iota-source-validation-service`

--- docker/iota-source-validation-service/build.sh
+++ docker/iota-source-validation-service/build.sh
@@ -2,4 +2,4 @@
 # Copyright (c) Mysten Labs, Inc.
 # Modifications Copyright (c) 2024 IOTA Stiftung
 # SPDX-License-Identifier: Apache-2.0
-./../utils/build-script.sh --container-name "iotaledger/iota-source-service"
+./../utils/build-script.sh --container-name "iotaledger/iota-source-validation-service"
