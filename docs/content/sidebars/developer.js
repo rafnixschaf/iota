75e54d0bf1f5d67e6e0190595f0b1f83ee07f295
salaheldinsoliman <49910731+salaheldinsoliman@users.noreply.github.com>
2024-11-13 12:17:46 +0100

    feat(docs): add ownership and references sections (#3911)
    * feat(docs): add ownership and references
    
    Signed-off-by: salaheldinsoliman <salaheldin_sameh@aucegypt.edu>
    
    * Apply suggestions from code review
    
    Co-authored-by: vivekjain23 <165671934+vivekjain23@users.noreply.github.com>
    
    * feat(docs): implement vivek's suggestion
    
    Signed-off-by: salaheldinsoliman <salaheldin_sameh@aucegypt.edu>
    
    * Apply suggestions from code review
    
    Co-authored-by: Lucas Tortora <85233773+lucas-tortora@users.noreply.github.com>
    
    * feat(docs): add title and description
    
    Signed-off-by: salaheldinsoliman <salaheldin_sameh@aucegypt.edu>
    
    * Update docs/content/developer/iota-101/move-overview/ownership-scope.mdx
    
    Co-authored-by: Lucas Tortora <85233773+lucas-tortora@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: salaheldinsoliman <salaheldin_sameh@aucegypt.edu>
    Co-authored-by: vivekjain23 <165671934+vivekjain23@users.noreply.github.com>
    Co-authored-by: Lucas Tortora <85233773+lucas-tortora@users.noreply.github.com>

--- docs/content/sidebars/developer.js
+++ docs/content/sidebars/developer.js
@@ -70,6 +70,8 @@ const developer = [
                             'developer/iota-101/move-overview/package-upgrades/custom-policies',
                         ],
                     },
+                    'developer/iota-101/move-overview/ownership-scope',
+                    'developer/iota-101/move-overview/references',
                     'developer/iota-101/move-overview/generics',
                     {
                         type: 'category',
