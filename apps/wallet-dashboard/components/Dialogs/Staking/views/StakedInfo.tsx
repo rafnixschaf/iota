80049ac1cceafc517b057e09a6c94dc8692a49a1
Eugene P <panteleymonchuk@gmail.com>
2024-11-19 18:18:19 +0200

    refactor(wallet-dashboard): streamline stake calculations and integrate new validator details utility

--- apps/wallet-dashboard/components/Dialogs/Staking/views/StakedInfo.tsx
+++ apps/wallet-dashboard/components/Dialogs/Staking/views/StakedInfo.tsx
@@ -1,16 +1,9 @@
 // Copyright (c) 2024 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
-import { useMemo } from 'react';
-import { IOTA_TYPE_ARG } from '@iota/iota-sdk/utils';
 import {
     formatPercentageDisplay,
-    calculateStakeShare,
-    useFormatCoin,
-    getTokenStakeIotaForValidator,
-    useGetDelegatedStake,
-    DELEGATED_STAKES_QUERY_REFETCH_INTERVAL,
-    DELEGATED_STAKES_QUERY_STALE_TIME,
     useValidatorInfo,
+    useGetStakingValidatorDetails,
 } from '@iota/core';
 import { KeyValueInfo, Panel, TooltipPosition } from '@iota/apps-ui-kit';
 
@@ -21,45 +14,20 @@ export function StakedInfo({
     validatorAddress: string;
     accountAddress: string;
 }) {
-    const { data: delegatedStake } = useGetDelegatedStake({
-        address: accountAddress || '',
-        staleTime: DELEGATED_STAKES_QUERY_STALE_TIME,
-        refetchInterval: DELEGATED_STAKES_QUERY_REFETCH_INTERVAL,
-    });
-    const { apy, isApyApproxZero, validatorSummary, system } = useValidatorInfo({
+    const { apy, isApyApproxZero } = useValidatorInfo({
         validatorAddress: validatorAddress,
     });
 
-    const totalValidatorsStake = useMemo(() => {
-        if (!system) return 0;
-        return system.activeValidators.reduce(
-            (acc, curr) => (acc += BigInt(curr.stakingPoolIotaBalance)),
-            0n,
-        );
-    }, [system]);
+    const { totalValidatorsStake, totalStakePercentage, totalStake } =
+        useGetStakingValidatorDetails({
+            accountAddress: accountAddress,
+            stakeId: null,
+            validatorAddress: validatorAddress,
+            unstake: false,
+        });
 
-    const totalStakePercentage = useMemo(() => {
-        if (!system || !validatorSummary) return null;
-
-        return calculateStakeShare(
-            BigInt(validatorSummary.stakingPoolIotaBalance),
-            BigInt(totalValidatorsStake),
-        );
-    }, [system, totalValidatorsStake, validatorSummary]);
-
-    const totalStake = useMemo(() => {
-        if (!delegatedStake) return 0n;
-        return getTokenStakeIotaForValidator(delegatedStake, validatorAddress);
-    }, [delegatedStake, validatorAddress]);
-
-    //TODO: verify this is the correct validator stake balance
-    const totalValidatorStake = validatorSummary?.stakingPoolIotaBalance || 0;
-
-    const [totalValidatorStakeFormatted, totalValidatorStakeSymbol] = useFormatCoin(
-        totalValidatorStake,
-        IOTA_TYPE_ARG,
-    );
-    const [totalStakeFormatted, totalStakeSymbol] = useFormatCoin(totalStake, IOTA_TYPE_ARG);
+    const [totalValidatorStakeFormatted, totalValidatorStakeSymbol] = totalValidatorsStake;
+    const [totalStakeFormatted, totalStakeSymbol] = totalStake;
 
     return (
         <Panel hasBorder>
