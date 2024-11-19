2a2157fd174516944e8cce6db203d693dfefcd48
Dr-Electron <dr-electr0n@protonmail.com>
2024-11-19 23:05:19 +0100

    Remove useless entries

--- docs/site/configs/preContent.tsx
+++ docs/site/configs/preContent.tsx
@@ -2,6 +2,5 @@ import React from 'react';
 import Admonition from '@theme/Admonition';
 
 export default {
-  '/about-iota*': <Admonition type='warning'>This is document  ation for v0.5, which is in alpha state.</Admonition>,
-  '/develop*': <Admonition type='warning'>Example Develop</Admonition>,
+  '/identity*': <Admonition type='warning'>This is documentation for vx.y, which is in alpha state.</Admonition>,
 };
