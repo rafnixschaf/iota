884098b343b5c3dcabd020bf6df462ea3e59a7f9
Dr-Electron <dr-electr0n@protonmail.com>
2024-11-19 19:22:06 +0100

    feat(docs): Allow custom components before DocItems

---
+++ docs/site/configs/preContent.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import Admonition from '@theme/Admonition';
+
+export default {
+  '/about-iota*': <Admonition type='warning'>This is document  ation for v0.5, which is in alpha state.</Admonition>,
+  '/develop*': <Admonition type='warning'>Example Develop</Admonition>,
+};
