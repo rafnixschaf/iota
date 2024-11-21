d32c948c60b054505ba99ed39f29ab81a68dc0d7
bingyanglin <bingyang.lin@iota.org>
2024-11-21 10:06:14 +0100

    Add get_transaction_checkpoint back

--- crates/iota-core/src/authority/authority_per_epoch_store.rs
+++ crates/iota-core/src/authority/authority_per_epoch_store.rs
@@ -679,6 +679,13 @@ impl AuthorityEpochTables {
         Ok(())
     }
 
+    pub fn get_transaction_checkpoint(
+        &self,
+        digest: &TransactionDigest,
+    ) -> IotaResult<Option<CheckpointSequenceNumber>> {
+        Ok(self.executed_transactions_to_checkpoint.get(digest)?)
+    }
+
     /// WARNING: This method is very subtle and can corrupt the database if used
     /// incorrectly. It should only be used in one-off cases or tests after
     /// fully understanding the risk.
